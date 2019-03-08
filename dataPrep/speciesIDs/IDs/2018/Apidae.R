ad230e9b986835807466e29b94929b592af1914b
Hamutahl Cohen <hamutahlc@eduroam-3-1-10-25-210-36.wnet.ucr.edu>
2019-03-08 13:05:45 -0800

    changed species ID

---
+++ dataPrep/speciesIDs/IDs/2018/Apidae.R
@@ -0,0 +1,184 @@
+
+sp.ids <- list(
+
+
+    # # Anthophora_montana_f = list(Genus="Anthophora", SubGenus="Mystacanthophora",
+                                # Species="montana", SubSpecies="", Sex="f", Author = "Cresson",
+                                # Determiner="L.C. Ponisio",
+
+                                # temp.id= c()),
+
+    # #Anthophora_montana_m = list(Genus="Anthophora", SubGenus="Mystacanthophora",
+                                # Species="montana", SubSpecies="", Sex="m", Author = "Cresson",
+                                # Determiner="L.C. Ponisio",
+
+                                # temp.id= c()),
+
+    # #Anthophora_terminalis_f = list(Genus="Anthophora", SubGenus="Clisodon",
+                                   # Species="terminalis",
+                                   # SubSpecies="", Sex="f",
+                                   # Author = "Cresson",
+                                   # Determiner="T. Griswold",
+
+                                   # temp.id= c()),
+
+    Apis_mellifera_f = list(Genus="Apis", SubGenus = "Apis", Species
+                            ="mellifera", SubSpecies="", Sex="f", Author = "Linnaeus",
+                            Determiner="L.C. Ponisio",
+
+                            temp.id= c("20410", "20411","20413","20414","20415","20416","20418","20419","20420","20421")),
+
+   # # Bombus_appositus_f = list(Genus = "Bombus", SubGenus="Subterraneobombus",
+                              # Species = "appositus", SubSpecies = "",
+                              # Sex ="f", Author = "Cresson",
+                              # Determiner="D. Yanega", 
+                              # temp.id = c()),
+
+    # #Bombus_appositus_m = list(Genus = "Bombus", SubGenus="Subterraneobombus",
+                              # Species = "appositus", SubSpecies = "",
+                              # Sex ="m", Author = "Cresson",
+                              # Determiner="D. Yanega",
+                              # temp.id = c()),
+
+   # # Bombus_bifarius_f = list(Genus = "Bombus", SubGenus="Pyrobombus", Species = "bifarius",
+                             # SubSpecies = "", Sex ="f", Author = "Cresson",
+                             # Determiner="D. Yanega",
+                             # temp.id =  c()),
+
+    # #Bombus_bifarius_m = list(Genus = "Bombus", SubGenus="Pyrobombus", Species = "bifarius",
+    						# SubSpecies = "", Sex ="m", Author = "Cresson", Determiner="D. Yanega", 
+    						# temp.id = c()),
+     
+	# #Bombus_centralis_f = list(Genus = "Bombus", SubGenus="Pyrobombus", Species = "centralis",
+                              # SubSpecies = "", Sex ="f", Author = "Cresson",
+                              # Determiner="D. Yanega",
+                            # temp.id = c()),
+
+   # # Bombus_fervidus_f = list(Genus = "Bombus", SubGenus="Thoracobombus", Species = "fervidus",
+                             # SubSpecies = "", Sex ="f", Author = "Fabricius",
+                             # Determiner="D. Yanega",
+                  			 # temp.id = c()),
+
+   # # Bombus_fervidus_m = list(Genus = "Bombus", SubGenus="Thoracobombus", Species = "fervidus",
+                             # SubSpecies = "", Sex ="m", Author = "Fabricius",
+                             # Determiner="D. Yanega",
+                             # temp.id = c()),
+
+    # #Bombus_flavifrons_f = list(Genus = "Bombus", SubGenus="Pyrobombus", Species = "flavifrons",
+                               # SubSpecies = "", Sex ="f", Author = "Cresson",
+                               # Determiner="D. Yanega",
+                             # temp.id = c()),
+                      
+   # # Bombus_flavifrons_m = list(Genus = "Bombus", SubGenus="Pyrobombus", Species =
+    							# "flavifrons", SubSpecies = "", Sex ="m", Author = "Cresson",
+    							# Determiner="D. Yanega",																									 # temp.id = c()),
+  
+	# #Bombus_huntii_f = list(Genus = "Bombus", SubGenus="Pyrobombus", Species = "huntii",
+                           # SubSpecies = "", Sex ="f", Author = "Greene",
+                           # Determiner="D. Yanega",
+                           # temp.id = c()),
+
+   # # Bombus_huntii_m = list(Genus = "Bombus", SubGenus="Pyrobombus", Species = "huntii", SubSpecies = "",
+                           # Sex ="m", Author = "Greene",
+                           # Determiner="D. Yanega",
+                           # temp.id = c()),
+
+    # #Bombus_morrisoni_f = list(Genus = "Bombus", SubGenus="Cullumanobombus", Species = "morrisoni",
+                              # SubSpecies = "", Sex ="f", Author = "Cresson",
+                              # Determiner="D. Yanega",
+
+                              # temp.id = c()),
+
+    # #Bombus_nevadensis_f = list(Genus = "Bombus", SubGenus="Bombias", Species =
+                                                                  # "nevadensis", SubSpecies = "", Sex ="f",
+                               # Author = "Cresson",
+                               # Determiner="D. Yanega",
+
+                               # temp.id = c()),
+
+    # #Bombus_nevadensis_m = list(Genus = "Bombus", SubGenus="Bombias",
+                               # Species = "nevadensis", SubSpecies = "", Sex ="m", Author = "Cresson",
+                               # Determiner="D. Yanega",
+
+                               # temp.id = c()),
+
+    # #Bombus_occidentalis_f = list(Genus = "Bombus", SubGenus="Bombus",
+                                 # Species = "occidentalis", SubSpecies = "", Sex ="f", Author = "Greene",
+                                 # Determiner="D. Yanega",
+
+                                # temp.id = c()),
+
+    # #Bombus_rufocinctus_f = list(Genus = "Bombus", SubGenus="Cullumanobombus", Species ="rufocinctus", SubSpecies = "", Sex ="f", Author = "Cresson",
+                                # Determiner="D. Yanega",
+
+                                # temp.id = c()),
+
+    # #Bombus_rufocinctus_m = list(Genus = "Bombus", SubGenus="Cullumanobombus",
+                                # Species ="rufocinctus", SubSpecies = "", Sex ="m", Author = "Cresson",
+                                # Determiner="D. Yanega",
+
+                               # temp.id = c()),
+                               
+   Bombus_sonorus_f = list(Genus = "Bombus", SubGenus="Thoracobombus",  Species = "sonorus",
+   				 SubSpecies = "", Sex ="f", Author = "Say", 
+   				 temp.id = c("20417")),
+    
+  # #Bombus_sonorus_m = list(Genus = "Bombus", SubGenus="Thoracobombus",  Species = "sonorus",
+    		# SubSpecies = "", Sex ="m", Author = "Say", 
+   			 #temp.id = c()),
+
+    # #Diadasia_australis_f = list(Genus="Diadasia", SubGenus="Coquillettapis",
+                                # Species="australis",
+                                # SubSpecies="", Sex="f",
+                                # Author = "Cresson",
+                                # Determiner="T. Griswold",
+
+                           # temp.id = c()),
+
+    # #Diadasia_australis_m = list(Genus="Diadasia", SubGenus="Coquillettapis",
+                                # Species="australis",
+                                # SubSpecies="", Sex="m",
+                                # Author = "Cresson",
+                                # Determiner="T. Griswold",
+
+                                # temp.id = c()),
+
+   # # Diadasia_diminuta_f = list(Genus="Diadasia", SubGenus="Coquillettapis",
+                               # Species="diminuta",
+                               # SubSpecies="", Sex="f",
+                               # Author = "Cresson",
+                               # Determiner="T. Griswold",
+
+                              # temp.id = c()),
+
+   # # Habropoda_salviarum_f = list(Genus="Habropoda", SubGenus ="",
+                                 # Species="salviarum", SubSpecies="", Sex="f", Author = "Cockerell",
+                                 # Determiner = "T. Griswold",
+
+                                # temp.id = c()),
+
+
+  # #  Xeromelecta_californica_f = list(Genus="Xeromelecta", SubGenus="Melectomorpha",
+                                     # Species="californica",
+                                     # SubSpecies="", Sex="f",
+                                     # Author = "Cresson",
+                                     # Determiner="T. Griswold",
+
+                                   # temp.id = c()),
+
+   # # Xeromelecta_californica_m = list(Genus="Xeromelecta", SubGenus="Melectomorpha",
+                                     # Species="californica",
+                                     # SubSpecies="",
+                                     # Sex="m", Author = "Cresson",
+                                     # Determiner="T. Griswold",
+
+                                     # temp.id = c()),
+
+    # # Xylocopa_californica_f = list(Genus="Xylocopa", SubGenus="Xylocopoides",
+                                  # Species="californica",
+                                  # SubSpecies="arizonensis", Sex="f",
+                                  # Author = "Cresson", Determiner="L.C. Ponisio",
+
+                         # temp.id = c()),
+
+)
