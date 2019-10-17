402945692c04643c8aa07e79f3fec5a2b2c4f4f4
Jamie Scholer <JamieScholer@bombus.dyn.ucr.edu>
2019-10-17 12:33:08 -0700

    cleaning up IDs

---
+++ dataPrep/speciesIDs/IDs/2018/Syrphidae.R
@@ -0,0 +1,249 @@
+
+sp.ids <- list(
+
+    Dasysyrphus_morpho1 = list(Genus = "Dasysyrphus", SubGenus="",
+                               Species = "sp.", SubSpecies = "a",
+                               Sex = "",
+                               Author = "",
+                               Determiner="M.Hauser",
+                               temp.id = c("18472", "18470")),
+
+
+    Copestylum_avidum_f = list(Genus="Copestylum", SubGenus="",
+                               Species="avidum",
+                               SubSpecies="", Sex="f",
+                               Author = "Sacken",
+                               Determiner="M.Hauser",
+                               temp.id= c("18229")),
+
+    Copestylum_avidum_m = list(Genus="Copestylum", SubGenus="",
+                               Species="avidum", SubSpecies="",
+                               Sex="m", Author = "Sacken",
+                               Determiner="M.Hauser",
+                               temp.id= c("18006","17980")),
+
+
+    Copestylum_lentum_f = list(Genus ="Copestylum",
+                               SubGenus="",
+                               Species =
+                                   "lentum", SubSpecies="", Sex="",
+                               Author ="Williston",
+                               Determiner="M.Hauser",
+                               temp.id = c("18487","17929","18093")),
+
+    Copestylum_lentum_m = list(Genus ="Copestylum",
+                               SubGenus="",
+                               Species =
+                                   "lentum", SubSpecies="", Sex="",
+                               Author ="Williston",
+                               Determiner="M.Hauser",
+                               temp.id = c("18232","18043")),
+
+
+    Copestylum_violaceum_f = list(Genus ="Copestylum",SubGenus="", Species
+                                  = "violaceum", SubSpecies="", Sex ="",
+                                  Author ="Say",
+                                  Determiner="M.Hauser",
+                                  temp.id = c("18231","18462","18019",
+                                              "17881","17981")),
+
+    Copestylum_violaceum_m = list(Genus ="Copestylum",SubGenus="", Species
+                                  = "violaceum", SubSpecies="", Sex ="",
+                                  Author ="Say",
+                                  Determiner="M.Hauser",
+                                  temp.id =c("17992","18174","17993",
+                                             "18175")),
+
+
+    Chrysotoxum_morpho1 = list(Genus="Chrysotoxum",SubGenus="", Species
+                               = "sp.", SubSpecies = "a",
+                               Sex = "", Author = "",
+                               Determiner="M.Hauser",
+                               temp.id =  c("18182","18588","18645",
+                                            "18763","18761","18226",
+                                            "17813","18484","17730")),
+
+    Toxomerus_marginatus_m = list(Genus = "Toxomerus",SubGenus="",
+                                  Species
+                                  = "marginatus", SubSpecies="",
+                                  Sex = "m",
+                                  Author ="Meigen",
+                                  Determiner="M.Hauser",
+                                  temp.id = c("17971","17891")),
+
+    Eristalis_hirta_f = list(Genus = "Eristalis",
+                             SubGenus="",
+                             Species = "hirta",
+                             SubSpecies="",
+                             Sex = "f", Author = "Loew",
+                             Determiner="M.Hauser",
+                             temp.id = c("18590")),
+
+
+    Eristalis_tenax_m = list(Genus = "Eristalis",
+                             SubGenus="",
+                             Species = "tenax",
+                             SubSpecies="", Sex = "",
+                             Author = "Linnaeus",
+                             Determiner="M.Hauser",
+                             temp.id =  c("18543","18589")),
+
+    Eristalis_stipator_f = list(Genus = "Eristalis", SubGenus="",
+                                Species = "stipator",
+                                SubSpecies="", Sex = "",
+                                Author = "Osten Sacken",
+                                Determiner="M.Hauser",
+                                temp.id = c("18020","18007","18225")),
+
+    Eristalis_stipator_m = list(Genus = "Eristalis", SubGenus="",
+                                Species = "stipator",
+                                SubSpecies="", Sex = "",
+                                Author = "Osten Sacken",
+                                Determiner="M.Hauser",
+                                temp.id = c("18169","17727","18046",
+                                            "17991","18012","18233")),
+
+
+
+    Eristalis_bellardii_f= list(Genus = "Belllardi", SubGenus="",
+                                Species = "bellardi", SubSpecies="",
+                                Sex = "", Author = "",
+                                Determiner="M.Hauser",
+                                temp.id = c("18087","18682")),
+
+
+    Didea_f = list(Genus = "Didea", SubGenus="", Species = "",
+                   SubSpecies="", Sex = "f", Author = "",
+                   Determiner="M.Hauser",
+                   temp.id = c("18163")),
+
+    Didea_fuscipes_m  = list(Genus = "Didea", SubGenus="",
+                             Species = "fuscipes",
+                             SubSpecies="", Sex = "m",
+                             Author = "Loew",
+                             Determiner="M.Hauser",
+                             temp.id = c("17740")),
+
+    Syrphus_sonorensis_m =list(Genus = "Syrphus",
+                               SubGenus="", Species = "sonorensis",
+                               SubSpecies="", Sex = "m",
+                               Author = "Vockeroth",
+                               Determiner="M.Hauser",
+                               temp.id =c("18396")),
+
+    Syrphus_sonorensis_f =list(Genus = "Syrphus", SubGenus="",
+                               Species = "sonorensis",
+                               SubSpecies="",
+                               Sex = "f", Author = "Vockeroth",
+                               Determiner="M.Hauser",
+                               temp.id =c("17692","17944")),
+
+    Syrphus_ribesii_f =list(Genus = "Syrphus", SubGenus="",
+                            Species = "ribesii", SubSpecies="",
+                            Sex = "f", Author = "Linnaeus",
+                            Determiner="M.Hauser",
+                            temp.id =c("17830","18681","18022" )),
+
+    Syrphus_opinator_f =list(Genus = "Syrphus", SubGenus="",
+                             Species = "opinator",
+                             SubSpecies="",
+                             Sex = "f",
+                             Author = "Osten Sacken",
+                             Determiner="M.Hauser",
+                             temp.id =c("18348","18430","18546")),
+
+    Cheilosia_morpho1 = list(Genus = "Cheilosia", SubGenus="",
+                             Species = "sp.",
+                             SubSpecies="a", Sex = "",
+                             Author = "", Determiner="M.Hauser",
+                             temp.id  =c("18116","18426")),
+
+    asemosyrphus_polygrammus_f = list(Genus="Asemosyrphus", SubGenus="",
+                                      Species="polygrammus", SubSpecies="",
+                                      Sex="f", Author = "",
+                                      Determiner="M.Hauser",
+                                      temp.id= c("17733")),
+
+    asemosyrphus_crandialli_f = list(Genus="Asemosyrphus", SubGenus="",
+                                     Species="crandialli", SubSpecies="",
+                                     Sex="f", Author = "",
+                                     Determiner="M.Hauser",
+                                     temp.id= c("18483")),
+
+    parasyrphus_f = list(Genus="Parasyrphus", SubGenus="",
+                         Species="relictus", SubSpecies="", Sex="f",
+                         Author = "Zetterstedt",
+                         Determiner="M.Hauser",
+                         temp.id= c("17836","17803","17794")),
+
+
+    paragus_m = list(Genus="Paragus", SubGenus="",
+                     Species="haemorrhous", SubSpecies="", Sex="m",
+                     Author = "",
+                     Determiner="M.Hauser",
+                     temp.id= c( "17732")),
+
+    paragus_f= list(Genus="Paragus", SubGenus="",
+                    Species="haemorrhous", SubSpecies="", Sex="f",
+                    Author = "",
+                    Determiner="M.Hauser",
+                    temp.id= c("17699","17779")),
+
+    neocnemodon_f = list(Genus="Neocnemodon", SubGenus="",
+                         Species="sp.", SubSpecies="a", Sex="f",
+                         Author = "",
+                         Determiner="M.Hauser",
+                         temp.id= c("17947")),
+
+    heringia_f= list(Genus="Heringia", SubGenus="",
+                     Species="", SubSpecies="", Sex="f", Author = "",
+                     Determiner="M.Hauser",
+                     temp.id= c("18539")),
+
+
+    eupeodes_m= list(Genus="Eupeodes", SubGenus="",
+                     Species="fumipennis", SubSpecies="", Sex="m",
+                     Author = "",
+                     Determiner="M.Hauser",
+                     temp.id= c("18434")),
+
+    eupeodes_f= list(Genus="Eupeodes", SubGenus="",
+                     Species="fumipennis", SubSpecies="", Sex="f",
+                     Author = "",
+                     Determiner="M.Hauser",
+                     temp.id= c("18550")),
+
+    eupeodes_f= list(Genus="Eupeodes", SubGenus="",
+                     Species="volucris", SubSpecies="", Sex="f",
+                     Author = "",
+                     Determiner="M.Hauser",
+                     temp.id= c( "17669")),
+
+    scaeva_f= list(Genus="Scaeva", SubGenus="",
+                   Species="affinis", SubSpecies="", Sex="f", Author = "",
+                   Determiner="M.Hauser",
+                   temp.id= c("18208","17804","18217","18061","18603",
+                              "18180", "18620","18714","18211",
+                              "18206","17837","17672", "17878",
+                              "18044","18040")),
+
+    scaeva_m= list(Genus="Scaeva", SubGenus="",
+                   Species="affinis", SubSpecies="", Sex="m", Author = "",
+                   Determiner="M.Hauser",
+                   temp.id= c("18479","18239")),
+
+    lapposyrphus_m= list(Genus="Lapposyrphus", SubGenus="",
+                         Species="lapponicus", SubSpecies="",
+                         Sex="m", Author = "",
+                         Determiner="M.Hauser",
+                         temp.id= c("18264","18181","17178")),
+
+    lapposyrphus_f= list(Genus="Lapposyrphus", SubGenus="",
+                         Species="lapponicus", SubSpecies="",
+                         Sex="f", Author = "",
+                         Determiner="M.Hauser",
+                         temp.id= c("18706","17843","18250","17703",
+                                    "18609","18602"))
+
+)
+
