a94b5a560da6515fcac1bdc6384857c1465fe802
lponisio <lponisio@gmail.com>
2019-12-04 13:07:38 -0800

    did some preliminary plotting of parnter varaibility, didn't see much interesting. Trying the possoit method next

--- analysis/variability/5plotting.R
+++ analysis/variability/5plotting.R
@@ -1,50 +1,16 @@
-## setwd('~/Dropbox/Yosemite')
+## setwd('~/Dropbox/skyIslands')
 rm(list=ls())
 setwd('analysis/variability')
-source('plotting/src/initialize.R')
 
-## ************************************************************
-## delta abund
-## ************************************************************
-load('saved/mods/drought.Rdata')
-delta <- delta$delta
+load('saved/results/partnerVar_Site.Rdata')
+beta.dist.site <- beta.dist
+load('saved/results/partnerVar_SiteYear.Rdata')
+beta.dist.site.year <- beta.dist
+load('saved/results/partnerVar_Year.Rdata')
+beta.dist.year <- beta.dist
 
-## network role
-dd.abund.var.pca <- expand.grid(var.pca1=seq(
-                                    from= min(delta$var.pca1),
-                                    to=max(delta$var.pca1),
-                                    length=10),
-                                simpson.div = mean(delta$simpson.div),
-                                beta.dist=mean(delta$beta.dist),
-                                deltaFloralAbund=
-                                    mean(delta$deltaFloralAbund),
-                                mean.pca1=
-                                    mean(delta$mean.pca1),
-                                deltaAbund=0)
+beta.dist <- rbind(beta.dist.site,
+                   beta.dist.site.year,
+                   beta.dist.year)
 
-abund.var.pca.pi <- predict.int(mod= mods,
-                                dd=dd.abund.var.pca,
-                                y="deltaAbund",
-                                family="guassian")
-
-## partner
-dd.abund.beta.dist <- expand.grid(beta.dist=seq(
-                                      from= min(delta$beta.dist),
-                                      to=max(delta$beta.dist),
-                                      length=10),
-                                  simpson.div = mean(delta$simpson.div),
-                                  var.pca1=mean(delta$var.pca1),
-                                  deltaFloralAbund=
-                                      mean(delta$deltaFloralAbund),
-                                  mean.pca1=
-                                      mean(delta$mean.pca1),
-                                  deltaAbund=0)
-abund.beta.dist.pi <- predict.int(mod= mods,
-                                  dd=dd.abund.beta.dist,
-                                  y="deltaAbund",
-                                  family="guassian")
-pdf.f(plotDeltaDiag,
-      file="figures/diagnostics/deltaAbund.pdf",
-      height=9, width=3)
-
-plotInteractionsPyroDiv()
+boxplot(beta.dist$dist~beta.dist$Type)
