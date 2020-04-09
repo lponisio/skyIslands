959bca3e81749d5c823463cb56afd39692108c5f
lponisio <lponisio@gmail.com>
2020-04-09 16:11:27 -0700

    fixed issues with syrphids

--- dataPrep/speciesIDs/IDs/2012/Colletidae.R
+++ dataPrep/speciesIDs/IDs/2012/Colletidae.R
@@ -1,32 +1,49 @@
 
 sp.ids <- list(
-  Hylaeus_annulatus = list(Genus="Hylaeus", SubGenus ="Hylaeus",
-    Species="annulatus", SubSpecies="", Sex="f", Author = "Linnaeus",
-    Determiner="J.S. Ascher",
-    temp.id= c("JC_071712_158", "JC_071612_23", "JC_071212_52",
-               "JC_071712_92", "JC_071612_125", "JC_071712_177",
-               "SC_072012_289", "SC_072712_171", "JC_071212_47",
-               "SC_072812_207", "SC_072812_210", "SC_072612_279")),
+    Hylaeus_annulatus = list(Genus="Hylaeus", SubGenus ="Hylaeus",
+                             Species="annulatus", SubSpecies="",
+                             Sex="f",
+                             Author = "Linnaeus",
+                             Determiner="J.S. Ascher",
+                             temp.id= c("JC_071712_158",
+                                        "JC_071612_23",
+                                        "JC_071212_52",
+                                        "JC_071712_92",
+                                        "JC_071612_125",
+                                        "JC_071712_177",
+                                        "SC_072012_289",
+                                        "SC_072712_171",
+                                        "JC_071212_47",
+                                        "SC_072812_207",
+                                        "SC_072812_210",
+                                        "SC_072612_279")),
 
-  Colletes_skinneri_f= list(Genus="Colletes", SubGenus ="",
-                            Species="skinneri", SubSpecies="", Sex="f", Author = "Viereck",
-                            Determiner="J.S. Ascher", temp.id=
-                                                          c("PL_081312_133",
-                                                          "MM_080412_4",
-                                                            "MM_080212_240")),
+    Colletes_skinneri_f= list(Genus="Colletes", SubGenus ="",
+                              Species="skinneri", SubSpecies="",
+                              Sex="f",
+                              Author = "Viereck",
+                              Determiner="J.S. Ascher",
+                              temp.id=
+                                  c("PL_081312_133",
+                                    "MM_080412_4",
+                                    "MM_080212_240")),
 
 
-  Colletes_gilensis= list(Genus="Colletes", SubGenus ="Productus",
-                          Species="gilensis", SubSpecies="", Sex="m", Author = "Cockerell",
-                          Determiner="J.S. Ascher",
-                          temp.id= c("SC_072712_58")),
+    Colletes_gilensis= list(Genus="Colletes", SubGenus ="Productus",
+                            Species="gilensis", SubSpecies="", Sex="m",
+                            Author = "Cockerell",
+                            Determiner="J.S. Ascher",
+                            temp.id= c("SC_072712_58")),
 
 
-  Colletes_slevini_m= list(Genus="Colletes", SubGenus ="",
-                           Species="slevini", SubSpecies="", Sex="m", Author = "Cockerell",
-                           Determiner="T. Griswold",
-                           temp.id= c("SC_072012_21", "SC_072612_134",
-                                      "SC_072612_133",
-                                      "SC_072012_312", "SC_072712_80"))
+    Colletes_slevini_m= list(Genus="Colletes", SubGenus ="",
+                             Species="slevini", SubSpecies="", Sex="m",
+                             Author = "Cockerell",
+                             Determiner="T. Griswold",
+                             temp.id= c("SC_072012_21",
+                                        "SC_072612_134",
+                                        "SC_072612_133",
+                                        "SC_072012_312",
+                                        "SC_072712_80"))
 
 )
