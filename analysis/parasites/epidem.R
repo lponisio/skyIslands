98fcfec003ddc2aa4dcd2e3b0364f74bd7f7cc00
Lauren Ponisio <lponisio@gmail.com>
2021-10-27 15:35:16 -0700

    worked on parasite analyses with R and N

--- analysis/parasites/epidem.R
+++ analysis/parasites/epidem.R
@@ -1,8 +1,17 @@
 ## setwd('~/Dropbox (University of Oregon)/skyIslands/')
 setwd('analysis/parasites')
 library(car)
+library(tidyverse)
 load('../../data/spec_RBCL_16s.Rdata')
 site.sum <- read.csv("../../data/sitestats.csv")
+veg <- read.csv("../../data/veg.csv")
+
+
+## setwd('analysis\\parasites')
+## load('..\\..\\data\\spec_RBCL_16s.Rdata')
+## site.sum <- read.csv("..\\..\\data\\sitestats.csv")
+
+## get data in the correct format
 
 parasites <- c("AspergillusSpp",
                "AscosphaeraSpp",
@@ -12,30 +21,42 @@ parasites <- c("AspergillusSpp",
                "NosemaBombi",
                "NosemaCeranae")
 
+## subset to the years we did the screenings
 spec <- spec[spec$Year == "2018",]
+
+## subset to the bees we screened and the screenings worked
 spec <- spec[spec$Apidae == 1 &
              !is.na(spec$Apidae),]
 
 spec <- merge(spec, site.sum)
 
+spec <- merge(spec, veg, all.x=TRUE)
+
+check1 <- spec[is.na(spec$FloralRichness)]
+
 table(spec$GenusSpecies)
 table(spec$Genus)
 
+## split up by genus
 bombus <- spec[spec$Genus == "Bombus",]
 table(bombus$Species)
+table(bombus$Site)
 
 megachile <- spec[spec$Genus == "Megachile",]
 table(megachile$Species)
+table(megachile$Site)
 
 anthophora <- spec[spec$Genus == "Anthophora",]
 table(anthophora$Species)
 
 apis <- spec[spec$Genus == "Apis",]
+table(apis$Site)
 
 
 ## check for any NAs
 apply(spec[, parasites], 2, function(x) sum(is.na(x)))
 
+## take means for plotting
 ## take the mean parasite prevalence for each species
 para.gensp <- spec %>%
     group_by(GenusSpecies, Site) %>%
@@ -53,8 +74,11 @@ boxplot(para.gensp$AscosphaeraSpp)
 boxplot(para.gensp$ApicystisSpp)
 boxplot(para.gensp$CrithidiaExpoeki)
 
+## some preliminary models to check out data
 
-bomb.mod <- glm(ParasitePresence ~ TotalAbundance + Richness,
+bomb.mod <- glm(ParasitePresence ~ scale(TotalAbundance) +
+                    scale(Richness) + scale(FloralRichness) +
+                    scale(FloralAbundance),
     data=bombus, family="binomial")
 summary(bomb.mod)
 
@@ -62,25 +86,32 @@ vif(bomb.mod)
 
 
 apis.mod <- glm(ParasitePresence ~ scale(TotalAbundance) +
-                    scale(Richness),
-    data=bombus, family="binomial")
+                    scale(Richness)  + scale(FloralRichness) +
+                    scale(FloralAbundance),
+    data=apis, family="binomial")
 summary(apis.mod)
 
 vif(apis.mod)
 
-anthophora.mod <- glm(ParasitePresence ~ TotalAbundance + Richness,
+anthophora.mod <- glm(ParasitePresence ~ TotalAbundance +
+                          Richness  + scale(FloralRichness) +
+                    scale(FloralAbundance) ,
     data=anthophora, family="binomial")
 summary(anthophora.mod)
 vif(anthophora.mod)
 
-megachile.mod <- glm(ParasitePresence ~ TotalAbundance + Richness,
+megachile.mod <- glm(ParasitePresence ~ TotalAbundance +
+                         Richness + scale(FloralRichness) +
+                    scale(FloralAbundance),
     data=megachile, family="binomial")
 summary(megachile.mod)
 vif(megachile.mod)
 
 
 
-all.mod <- glm(ParasitePresence ~ TotalAbundance + Richness,
+all.mod <- glm(ParasitePresence ~ TotalAbundance + Richness +
+                   scale(FloralRichness) +
+                    scale(FloralAbundance),
     data=spec, family="binomial")
 summary(all.mod)
 
