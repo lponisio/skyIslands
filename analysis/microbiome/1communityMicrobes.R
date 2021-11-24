17a066e03dbdf1a242675da897cec3ded7ba2201
Lauren Ponisio <lponisio@gmail.com>
2021-11-24 09:29:57 -0800

    started microbiome analysis

---
+++ analysis/microbiome/1communityMicrobes.R
@@ -0,0 +1,9 @@
+## setwd('/Volumes/bombus/Dropbox (University of Oregon)/skyislands')
+
+## setwd('~/Dropbox (University of Oregon)/skyislands')
+
+setwd("analysis/microbiome")
+rm(list=ls())
+
+source("src/init.R")
+source("src/misc.R")
---
+++ analysis/microbiome/src/init.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+library(brms)
+## library(tidybayes)
+library(tidyverse)
+
+
+load('../../data/spec_RBCL_16s.Rdata')
+site.sum <- read.csv("../../data/sitestats.csv")
+veg <- read.csv("../../data/veg.csv")
+load("../../data/trees.RData")
+
+parasites <- c("AspergillusSpp",
+               "AscosphaeraSpp",
+               "ApicystisSpp",
+               "CrithidiaExpoeki",
+               "CrithidiaBombi",
+               "NosemaBombi",
+               "NosemaCeranae")
+
+spec <- merge(spec, site.sum)
+
+spec <- merge(spec, veg, all.x=TRUE)
