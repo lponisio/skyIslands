fdfab4d9cbe6e662d2928b7ca710a4c657918b41
hayesrebecca <rhayes7@uoregon.edu>
2022-11-08 15:34:04 -0800

    function to calc model fit by bee genus working, about to run on talapas for more iterations

--- analysis/microbiome/src/init.R
+++ analysis/microbiome/src/init.R
@@ -1,13 +1,14 @@
 library(ggplot2)
 library(brms)
 ## library(tidybayes)
-library(tidyverse)
+## library(tidyverse)
+library(dplyr)
 
 
 load('../../data/spec_RBCL_16s.Rdata')
 site.sum <- read.csv("../../data/sitestats.csv")
 veg <- read.csv("../../data/veg.csv")
-load("../../data/trees.RData")
+load("../../data/trees.Rdata")
 
 parasites <- c("AspergillusSpp",
                "AscosphaeraSpp",
