34284baa2f9099a7b79c420f19bcace76b81fe57
santillanaale <119963666+santillanaale@users.noreply.github.com>
2023-01-25 11:49:46 -0800

    Update Apidae.R

--- dataPrep/speciesIDs/IDs/2022/Apidae.R
+++ dataPrep/speciesIDs/IDs/2022/Apidae.R
@@ -824,6 +824,16 @@ sp.ids <- list(
                                          37224, 37223, 37222, 37221, 37312,
                                          37298, 37297, 37296, 37269, 37294,
                                          37293, 37291, 37159, 37157, 37158))
+    
+
+     Andrena_hitei_f= list(Genus="Andrena",
+                                 SubGenus ="",
+                                 Species="hitei",
+                                 SubSpecies="",
+                                 Sex="f",
+                                 Author = "Cockerell",
+                                 Determiner="J.L. Neff",
+                                 temp.id= c(36498, 36499))
 
 
 )
