ce81caf605ac0f663bc91385a18f0670ddaf1479
santillanaale <119963666+santillanaale@users.noreply.github.com>
2023-01-31 11:40:34 -0800

    Created Colletidae.R and entered Neff IDs

---
+++ dataPrep/speciesIDs/IDs/2022/Colletidae.R
@@ -0,0 +1,10 @@
+
+
+    Colletes_eulophi_f= list(Genus="Colletes",
+                             SubGenus="",
+                             Species="eulophi",
+                             SubSpecies="", Sex="f",
+                             Author = "Robertson",
+                             Determiner="J.L. Neff",
+                             temp.id= c(38343, 38346))
+    )
