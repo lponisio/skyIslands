ff23fa0f6f8bb8d0b4b8eaf7c134cf6e8703ce7c
santillanaale <119963666+santillanaale@users.noreply.github.com>
2023-02-03 11:26:31 -0800

    Create Bombyliidae.R

---
+++ dataPrep/speciesIDs/IDs/2021/Bombyliidae.R
@@ -0,0 +1,70 @@
+sp.ids <- list(
+
+    villa_sp_b = list(Genus="Villa",
+                      SubGenus ="",
+                      Species="sp.",
+                      SubSpecies="b",
+                      Sex="",
+                      Author =  "",
+                      Determiner="L. Ponisio",
+                      temp.id= c()),
+
+    exoprosopa_sp = list(Genus="Exoprosopa",
+                         SubGenus ="",
+                         Species="sp.",
+                         SubSpecies="a",
+                         Sex="",
+                         Author =  "",
+                         Determiner="L. Ponisio",
+                         temp.id= c()),
+
+
+    villa_sp_a = list(Genus="Villa",
+                      SubGenus ="",
+                      Species="sp.",
+                      SubSpecies="a",
+                      Sex="",
+                      Author =  "",
+                      Determiner="L. Ponisio",
+                      temp.id= c()),
+
+    villa_sp_c = list(Genus="Villa",
+                      SubGenus ="",
+                      Species="sp.",
+                      SubSpecies="c",
+                      Sex="",
+                      Author =  "",
+                      Determiner="L. Ponisio",
+                      temp.id= c()),
+
+    hemipenthes_sp =
+
+    list(Genus="Hemipenthes", SubGenus ="", Species="sp.",
+         SubSpecies="", Sex="", Author = "",
+         Determiner="L. Ponisio",
+         temp.id= c()),
+
+    bombylius_sp_a = list(Genus="Bombylius",
+                          SubGenus ="Bombylius",
+                          Species="sp.",
+                          SubSpecies="a",
+                          Sex="",
+                          Author =  "",
+                          Determiner="L. Ponisio",
+                          temp.id= c()),
+
+
+    Antrax_sp_a = list(Genus="Antrax",
+                       SubGenus ="",
+                       Species="sp.",
+                       SubSpecies="a",
+                       Sex="",
+                       Author =  "",
+                       Determiner="L. Ponisio",
+                       temp.id= c())
+
+
+
+
+
+)
