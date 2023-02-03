59e8b48c6bf5630e6eb38db62ef8064641939541
santillanaale <119963666+santillanaale@users.noreply.github.com>
2023-02-03 12:08:10 -0800

    Entered Villa and Hemipenthes

--- dataPrep/speciesIDs/IDs/2021/Bombyliidae.R
+++ dataPrep/speciesIDs/IDs/2021/Bombyliidae.R
@@ -26,7 +26,17 @@ sp.ids <- list(
                       Sex="",
                       Author =  "",
                       Determiner="L. Ponisio",
-                      temp.id= c()),
+                      temp.id= c(36997, 37010, 36985, 37038, 36973,
+                                36776, 37140, 37126, 37108, 37125,
+                                36950, 37093, 37013, 37761, 37894,
+                                37905, 37931, 37832, 37833, 37645,
+                                37573, 30976, 30988, 31001,
+                                30844, 30845, 30847, 30955, 30965,
+                                30701, 32422, 32448, 32614, 33296,
+                                33177, 33292, 33294, 33886, 33304, 
+                                32620, 31829, 31631, 31628, 31638,
+                                31801, 35060, 36507, 36509, 36510,
+                                36558, 36967)),
 
     villa_sp_c = list(Genus="Villa",
                       SubGenus ="",
@@ -37,12 +47,17 @@ sp.ids <- list(
                       Determiner="L. Ponisio",
                       temp.id= c()),
 
-    hemipenthes_sp =
-
-    list(Genus="Hemipenthes", SubGenus ="", Species="sp.",
-         SubSpecies="", Sex="", Author = "",
-         Determiner="L. Ponisio",
-         temp.id= c()),
+    hemipenthes_sp = list(Genus="Hemipenthes", 
+                          SubGenus ="", 
+                          Species="sp.",
+                          SubSpecies="", 
+                          Sex="", 
+                          Author = "",
+                          Determiner="L. Ponisio",
+                          temp.id= c(31161, 31163, 31164, 31315, 32156,
+                                    32429, 32434, 32435, 32449, 32456,
+                                    32457, 32460, 32458, 32625, 32791,
+                                    34053, 33301, 34079, 34080)),
 
     bombylius_sp_a = list(Genus="Bombylius",
                           SubGenus ="Bombylius",
@@ -53,6 +68,16 @@ sp.ids <- list(
                           Determiner="L. Ponisio",
                           temp.id= c()),
 
+    
+    bombylius_sp_b = list(Genus="Bombylius",
+                          SubGenus ="Bombylius",
+                          Species="sp.",
+                          SubSpecies="b",
+                          Sex="",
+                          Author =  "",
+                          Determiner="L. Ponisio",
+                          temp.id= c(32451, 31317, 34793)),
+    
 
     Antrax_sp_a = list(Genus="Antrax",
                        SubGenus ="",
@@ -61,7 +86,8 @@ sp.ids <- list(
                        Sex="",
                        Author =  "",
                        Determiner="L. Ponisio",
-                       temp.id= c())
+                       temp.id= c(34289, 34141, 33525, 34288, 35227,
+                                 33765))
 
 
 
