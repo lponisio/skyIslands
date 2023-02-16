d60fe0ab0fbf157c748bb5c6f821f557ef5f97d8
santillanaale <119963666+santillanaale@users.noreply.github.com>
2023-02-16 15:34:59 -0800

    Create Stratiomyidae.R

---
+++ dataPrep/speciesIDs/IDs/2017/Stratiomyidae.R
@@ -0,0 +1,12 @@
+sp.ids <- list(
+
+    Adoxomyia_sp_a = list(Genus="Adoxomyia",
+                      SubGenus ="",
+                      Species="sp.",
+                      SubSpecies="a",
+                      Sex="",
+                      Author =  "",
+                      Determiner="M. Hauser",
+                      temp.id= c(23626))
+                                 
+)
