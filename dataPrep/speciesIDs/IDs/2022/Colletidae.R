21c1c5c13c9bf8a659726dd7ce41a084463ff0f2
jzorn94 <82061963+jzorn94@users.noreply.github.com>
2023-03-03 12:12:28 -0800

    Update Colletidae.R
    entered colletes wootoni

--- dataPrep/speciesIDs/IDs/2022/Colletidae.R
+++ dataPrep/speciesIDs/IDs/2022/Colletidae.R
@@ -8,6 +8,14 @@ sp.ids <- list(
                              Determiner="J.L. Neff",
                              temp.id= c(38343, 38346))
     
+    Colletes_wootoni_f= list(Genus="Colletes",
+                             SubGenus="",
+                             Species="wootoni",
+                             SubSpecies="", Sex="f",
+                             Author = "Cockerell",
+                             Determiner="J.L. Neff",
+                             temp.id= c(30925, 30780, 30930, 36741, 30777))
+    
     Hylaeus_annulatus_f= list(Genus="Hylaeus",
                              SubGenus="",
                              Species="annulatus",
