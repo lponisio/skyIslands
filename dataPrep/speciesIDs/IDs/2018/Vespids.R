04d92ef8f0dffc81707945bab0cfe56b36a2a824
santillanaale <119963666+santillanaale@users.noreply.github.com>
2023-03-08 13:19:49 -0600

    Entered Carpenter IDs

--- dataPrep/speciesIDs/IDs/2018/Vespids.R
+++ dataPrep/speciesIDs/IDs/2018/Vespids.R
@@ -1,6 +1,24 @@
 
 sp.ids <- list(
 
+  bustamente_f = list(Genus="Ancistrocerus", SubGenus ="",
+     Species="bustamente", SubSpecies="", Sex="f", Author = "Saussure",
+     Determiner="J. Carpenter", temp.id
+     =c("17704","18253","17938","18154","17854","18455","18501","BBSL1058974",
+        "BBSL1070257","BBSL1059156","BBSL1059191","BBSL1059038","BBSL1059334",
+        "BBSL1059435","BBSL1070357","BBSL1070358","BBSL1085213","BBSL1085147",
+        "BBSL1085245","BBSL1085174","BBSL1085172","BBSL1085153","BBSL1074458",
+        "BBSL1071379","BBSL1071375","BBSL1071530","BBSL1058371")),
+  
+  bustamente_m = list(Genus="Ancistrocerus", SubGenus ="",
+     Species="bustamente", SubSpecies="", Sex="m", Author = "Saussure",
+     Determiner="J. Carpenter", temp.id
+     =c("BBSL1085173","BBSL1085168","BBSL1085099","BBSL1085182",
+        "BBSL1085196","BBSL1059105","BBSL1070260","BBSL1070236",
+        "BBSL1085148","BBSL1085154","BBSL1058256","BBSL1086515",
+        "BBSL1086438","BBSL1086445","BBSL1058584","BBSL1058573",
+        "BBSL1058485")),
+  
   Scoliidae = list(Genus="Scoliidae", SubGenus ="",
    Species="sp.", SubSpecies="a", Sex="f", Author = "",
     Determiner="J. Carpenter", temp.id
