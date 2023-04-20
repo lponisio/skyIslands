6fb023a404ae3dfc5f9c61c8142f4afc8a8655a7
jzorn94 <82061963+jzorn94@users.noreply.github.com>
2023-04-20 10:25:41 -0700

    Update Butterflies.R

--- dataPrep/speciesIDs/IDs/2018/Butterflies.R
+++ dataPrep/speciesIDs/IDs/2018/Butterflies.R
@@ -5,3 +5,7 @@ Hesperia_comma = list(Genus = "Hesperia", SubGenus="",Species =
  Hesperia_pahaska = list(Genus = "Hesperia", SubGenus="",Species =
     "pahaska", SubSpecies = "", Sex = "", Author = "Leussler", Family
     = "Hesperiidae", Determiner="L.Ponisio", temp.id = c(24264,24261,24805,24808)),
+
+Speyeria_atlantis = list(Genus = "Speyeria", SubGenus="", Species =
+    "atlantis", SubSpecies = "", Sex = "", Author = "Edwards", Family
+    = "Nymphalidae", Determiner="L.Ponisio", temp.id = c(24891,24914)),
