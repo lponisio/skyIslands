839751c4e8a0291f206527d294d25068bfa87acc
jzorn94 <82061963+jzorn94@users.noreply.github.com>
2023-04-20 10:18:06 -0700

    Update and rename Lepidoptera.R to Butterflies.R

---
+++ dataPrep/speciesIDs/IDs/2017/Butterflies.R
@@ -0,0 +1,33 @@
+Hesperia_comma = list(Genus = "Hesperia", SubGenus="",Species =
+    "comma", SubSpecies = "", Sex = "", Author = "Linnaeus", Family =
+    "Hesperiidae", Determiner="L.Ponisio", temp.id = c(24886,24811)),
+    
+    Hesperia_pahaska = list(Genus = "Hesperia", SubGenus="",Species =
+    "pahaska", SubSpecies = "", Sex = "", Author = "Leussler", Family
+    = "Hesperiidae", Determiner="L.Ponisio", temp.id = c(34262,23658,24165,30054,24807,24164,24177,24264,24261,24805,24808)),
+
+Burnsius_communis = list(Genus = "Burnsius", SubGenus="", Species =
+    "communis", SubSpecies = "", Sex = "", Author = "Grote", Family =
+    "Hesperiidae", Determiner="L.Ponisio", temp.id =c(21574,24907,24906,23867,24766,24224,24227,21575,24816,21557,21501,24150,21480,30046,30118,23736,23640,24902)),
+
+  Thorybes_pylades = list(Genus = "Thorybes", SubGenus="", Species =
+    "pylades", SubSpecies = "", Sex = "", Author = "Scudder", Family =
+    "Hesperiidae", Determiner="L.Ponisio", temp.id = c(24178)),
+
+Epargyreus_clarus = list(Genus = "Epargyreus", SubGenus="",Species =
+    "clarus", SubSpecies = "", Sex = "", Author = "Cramer", Family =
+    "Hesperiidae", Determiner="L.Ponisio", temp.id =c(24259,23657)),
+
+  Autochton_cellus = list(Genus = "Autochton", SubGenus="", Species =
+    "cellus", SubSpecies = "", Sex = "", Author =
+    "Boisduval & Leconte", Family = "Hesperiidae", Determiner="L.Ponisio", temp.id
+    =c(23671)),
+
+Atrytonopsis_ovinia_zaovinia = list(Genus = "Atrytonopsis", SubGenus="", Species =
+    "ovinia", SubSpecies = "zaovinia", Sex = "", Author =
+    "Dyar", Family = "Hesperiidae", Determiner="L.Ponisio", temp.id
+    =c(21534,30069,24171,24203,24197)),
+
+
+    
+    
