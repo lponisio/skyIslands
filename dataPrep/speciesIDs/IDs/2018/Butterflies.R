8017305d60c442b0a6febc5f3b4afd8645885bd0
jzorn94 <82061963+jzorn94@users.noreply.github.com>
2023-04-20 10:18:56 -0700

    Create Butterflies.R

---
+++ dataPrep/speciesIDs/IDs/2018/Butterflies.R
@@ -0,0 +1,3 @@
+Hesperia_comma = list(Genus = "Hesperia", SubGenus="",Species =
+    "comma", SubSpecies = "", Sex = "", Author = "Linnaeus", Family =
+    "Hesperiidae", Determiner="L.Ponisio", temp.id = c(24886)),
