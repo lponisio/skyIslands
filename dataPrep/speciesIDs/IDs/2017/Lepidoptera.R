92f3c44b7f29bcb74708fca2314646a9bf5f2305
jzorn94 <82061963+jzorn94@users.noreply.github.com>
2023-04-20 10:01:48 -0700

    Create Lepidoptera.R

---
+++ dataPrep/speciesIDs/IDs/2017/Lepidoptera.R
@@ -0,0 +1,9 @@
+Hesperia_comma = list(Genus = "Hesperia", SubGenus="",Species =
+    "comma", SubSpecies = "", Sex = "", Author = "Linnaeus", Family =
+    "Hesperiidae", Determiner="L.Ponisio", temp.id = c(24886,24811)),
+    
+    Hesperia_pahaska = list(Genus = "Hesperia", SubGenus="",Species =
+    "pahaska", SubSpecies = "", Sex = "", Author = "Leussler", Family
+    = "Hesperiidae", Determiner="L.Ponisio", temp.id = c(34262,23658,24165,30054,24807,24164,24177,24264,24261,24805,24808)),
+    
+    
