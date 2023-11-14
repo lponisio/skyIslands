8c3ea48b522c43300c5f6a6974c5f458d72f9914
hayesrebecca <rhayes7@uoregon.edu>
2023-11-14 11:05:41 -0800

    got tree code working with updated full dataset

--- dataPrep/6_1_treeBuilding.R
+++ dataPrep/6_1_treeBuilding.R
@@ -1,14 +1,20 @@
 
 ## tutorial: https://bioconductor.org/packages/devel/bioc/vignettes/ggtreeExtra/inst/doc/ggtreeExtra.html
-
+#rm(list=ls())
 
 #####
 setwd('~/Dropbox (University of Oregon)/skyIslands/')
 
 
 src('dataPrep/src/trees_init.R')
+load('data/presAbsTable.Rdata')
+load('data/spec_RBCL_16s.Rdata')
+load('data/physeq16s.Rdata')
 ##import community presence/absence file
 setwd("../skyIslands_saved")
+
+meta_cols <- c('UniqueID', 'Family', 'Genus', 'Species', 'GenusSpecies', 'Sex', 'Site', 'Meadow')
+
 
 
 
@@ -23,25 +29,24 @@ setwd("../skyIslands_saved")
 
 ##probs want to make sure each site in each individual graph is colored uniformly for all graphs 
 
-phylotree_heatmap_byGenus <- function(tree.object, metadata, this.genus, presAbsTable, site.order, all_levels=TRUE, levels_to_drop, clade_names=NULL, do_collapse=FALSE, tree.type){
+phylotree_heatmap_byGenus <- function(tree.object, metadata, this.species, presAbsTable, site.order, all_levels=TRUE, levels_to_drop, clade_names=NULL, do_collapse=FALSE){
   #filter to include just the unique IDs in the specified genus
-  genus_ids <- metadata %>%
-    filter(Genus==this.genus) %>%
+  sp_ids <- metadata %>%
+    filter(GenusSpecies==this.species) %>%
     select(UniqueID)
   
   #pull out all sites that include the specified genus
-  my_sites <- unique(metadata$Site[metadata$Genus==this.genus])
+  my_sites <- unique(metadata$Site[metadata$GenusSpecies==this.species])
 
   #remove tips from the tree that are not in the list of unique IDs in the specified genus
-  trimmed_tree <- prune_samples(sample_names(tree.object) %in% genus_ids$UniqueID, tree.object)
+  trimmed_tree <- prune_samples(sample_names(tree.object) %in% sp_ids$UniqueID, tree.object)
   
   #remove taxa from the tree where there were less than zero observations
   pruned_tree <- prune_taxa(taxa_sums(trimmed_tree) > 0, trimmed_tree)
   
-  if(tree.type=='16s'){
   #read in the taxonomic info for each feature
   feature.2.tax.16s <-
-    read.table("SI_pipeline/merged/16s/taxonomy16s.txt", sep="\t",
+    read.table("SI_pipeline/R2018/2023_sequence_results_raw/merged/16s/taxonomy.tsv", sep="\t",
                header=TRUE)
   
   #make labels from the taxonomic info
@@ -140,10 +145,10 @@ phylotree_heatmap_byGenus <- function(tree.object, metadata, this.genus, presAbs
   row.names(matched_pres_meta) <- matched_id
   
   meta_match_sites <- match_shared_ID(metadata, matched_pres_meta) %>%
-    select(UniqueID, Site, Genus) %>%
+    select(UniqueID, Site, GenusSpecies) %>%
     mutate(Site = factor(Site)) %>%
-    filter(Genus==this.genus) %>%
-    select(!Genus) %>%
+    filter(GenusSpecies==this.species) %>%
+    select(!GenusSpecies) %>%
     group_by(UniqueID, Site) %>%
     count() %>%
     pivot_wider(names_from=Site,
@@ -196,176 +201,9 @@ phylotree_heatmap_byGenus <- function(tree.object, metadata, this.genus, presAbs
                   fill=SiteCount, width=0.1),
       show.legend=TRUE) +
     scale_fill_viridis(option="rocket", discrete=FALSE, direction=-1) +
-    ggtitle(paste(tree.type, this.genus, this_level)) 
+    ggtitle(paste(this.species, this_level)) 
   p2
-  } else if (tree.type=='rbcl'){
-    #read in the taxonomic info for each feature
-    feature.2.tax.rbcl <-
-      read.table("SI_pipeline/merged/RBCL/taxonomyRBCL.txt", sep="\t",
-                 header=TRUE)
-    
-    ## ## R knows to ignore the row in the txt with a # (see defaults read.table)
-    
-    ## add 16s/rbcl to make the columns easy to find when they are added
-    ## to the specimen data
-    
-    feature.2.tax.rbcl$Taxon  <- gsub(" ", "_", feature.2.tax.rbcl$Taxon)
-    feature.2.tax.rbcl$Taxon <- paste("RBCL", feature.2.tax.rbcl$Taxon,
-                                      sep=':')
-    
-    gentree <- phy_tree(pruned_tree, errorIfNULL=TRUE)
-    orig_len <- length(gentree$tip.label)
-    print(paste('original # tips', orig_len))
-    
-    ## in the future do the same for other runs
-    ## tree.rbclR1 <- phy_tree(physeqRBCLR1, errorIfNULL=TRUE)
-    
-    ## match the tip labs to the table with feature ID and Taxon
-    gentree$tip.label  <-  feature.2.tax.rbcl$Taxon[
-      match(gentree$tip.label,
-            feature.2.tax.rbcl$FeatureID)]
-    
-    #browser()
-    
-    #print(length(gentree$tip.label))
-    if (do_collapse == TRUE){
-      #pull out unique tip labels
-      groups <- unique(gentree$tip.label)
-      
-      #collapse branches that have the same label
-      for (this.group in groups){
-        gentree <- collapse_identical_tips(gentree, this.group)
-      }
-    }
-    
-    #print(length(gentree$tip.label))
-    #browser()
-    
-    # if(all_levels==FALSE){
-    # for (level in levels_to_drop) {
-    #       tips_to_drop <- grep(level, gentree$tip.label)
-    #       print(paste('tip drop len', length(tips_to_drop)))
-    #       if (length(tips_to_drop) > 0) {
-    #         gentree <- drop.tip(gentree, tips_to_drop)
-    #         new_len <- length(gentree$tip.label)
-    #         percent_drop <- (length(tips_to_drop)/orig_len)*100
-    #         print(paste(level, "percent tips dropped", percent_drop))
-    #         final_level <- level
-    #       }
-    #     }
-    # }
-    # if (all_levels == FALSE) {
-    #   for (level in levels_to_drop) {
-    #     if (level == 'species'){
-    #       drop.level <- '_(?:[^_]+)$'
-    #     } else if (level == 'genus') {
-    #       drop.level <- '_(?:[^_]+)$'
-    #     }
-    #     #make a list of tip labels that include the specified level
-    #     tips_to_modify <- grep(drop.level, gentree$tip.label)
-    #     if (length(tips_to_modify) > 0) {
-    #       # Replace the pattern and everything following it with an empty string
-    #       gentree$tip.label[tips_to_modify] <- gsub(drop.level, "", gentree$tip.label[tips_to_modify])
-    #       gentree$tip.label[tips_to_modify] <- gsub("_sp.", "", gentree$tip.label[tips_to_modify])
-    #       gentree$tip.label[tips_to_modify] <- gsub("_$", "", gentree$tip.label[tips_to_modify])
-    #       this.tip.level.label <- paste(level)
-    #       new_len <- length(gentree$tip.label)
-    #       percent_drop <- (length(tips_to_modify) / orig_len) * 100
-    #       print(paste(level, "percent tips dropped", percent_drop))
-    #       final_level <- level
-    #     }
-    #   }
-    # }
-    
-    #browser()
-    #print(length(gentree$tip.label))
-    if (do_collapse == TRUE){
-      #pull out unique tip labels
-      original_nodes <- gentree %>% as_tibble() 
-      groups <- unique(gentree$tip.label)
-      #browser()
-      
-      #collapse branches that have the same label
-      for (this.group in groups){
-        gentree <- collapse_identical_tips(gentree, this.group)
-      }
-      new_nodes <- gentree %>% as_tibble()
-    }
-    #print(length(gentree$tip.label))
-    #browser()
-    
-    if(all_levels==FALSE){
-      this_level <- paste(": Collapsed")
-    } else {this_level <- ': Full Tree'}
-    
-    
-    matched_presabs <- match_shared_tiplabels(gentree, presAbsTable)
-    
-    matched_pres_meta <- match_shared_ID(matched_presabs, metadata)
-    
-    matched_id <- matched_pres_meta$UniqueID
-    row.names(matched_pres_meta) <- matched_id
-    
-    meta_match_sites <- match_shared_ID(metadata, matched_pres_meta) %>%
-      select(UniqueID, Site, Genus) %>%
-      mutate(Site = factor(Site)) %>%
-      filter(Genus==this.genus) %>%
-      select(!Genus) %>%
-      group_by(UniqueID, Site) %>%
-      count() %>%
-      pivot_wider(names_from=Site,
-                  values_from = n,
-                  names_expand = TRUE,
-                  id_expand=TRUE) %>%
-      pivot_longer(cols=2:length(colnames(.)),
-                   names_to='Site',
-                   values_to='Site_present') %>%
-      filter(Site_present > 0) #%>%
-    #mutate(Site = factor(Site, levels=site.order))
-    #browser()
-    
-    features_site_metadata <- match_shared_ID(matched_pres_meta, meta_match_sites) %>%
-      right_join(meta_match_sites, by='UniqueID') %>%
-      pivot_longer(cols = starts_with('RBCL'), names_to = 'pollen', values_to = 'pollen_pres') %>%
-      group_by(pollen) %>%
-      filter(pollen_pres == 1) %>%
-      select(!pollen_pres) %>%
-      relocate(pollen) %>%
-      group_by(pollen) %>%
-      add_count(Site, name="n_individuals") %>%
-      mutate(SiteCount = as.numeric(n_distinct(Site)))
-    
-    tree_tip_labs <- gentree$tip.label
-    
-    #dropping branches that weren't in the presence abs table
-    final_drop <- gentree$tip.label[!(gentree$tip.label %in% features_site_metadata$pollen)]
-    #browser()
-    
-    if (length(final_drop) > 0){
-      gentree <- drop.tip(gentree, final_drop)
-    }
-    
-    p <- ggtree(gentree, layout='rectangular') 
-    p
-    
-    p2 <- p +
-      new_scale_fill() + 
-      geom_tiplab(align=TRUE, size=2) + 
-      coord_cartesian(clip="off") +
-      geom_fruit(
-        data=features_site_metadata,
-        geom=geom_tile,
-        pwidth=0.1,
-        offset=0.5,
-        mapping=aes(y=pollen,
-                    #x=SiteCount,
-                    fill=SiteCount, width=0.1),
-        show.legend=TRUE) +
-      scale_fill_viridis(option="mako", discrete=FALSE, direction=-1) +
-      ggtitle(paste(tree.type, this.genus)) 
-    p2
-    
-  }
+  
   #browser()
 }
 
@@ -377,30 +215,17 @@ phylotree_heatmap_byGenus <- function(tree.object, metadata, this.genus, presAbs
 # what i will try next will be adding an index to keep track of the branch location and then after that dropping down the taxonomy and using the tip index to assign site count but surely this will be more complicated than that... :/ 
 
 #tree.type <- 'rbcl'
-tree.type <- '16s'
 
-if(tree.type=='16s'){
-  meta <- spec16s %>%
+meta <- spec.net %>%
+  filter(Apidae == 1) %>%
     select(all_of(meta_cols), Apidae, starts_with('16s')) %>%
     na.omit() %>%
-    select(!starts_with('X16s')) %>%
-    filter(Apidae == 1) #%>%
+    select(!starts_with('16s')) 
 
-  comm_presabs <- as.data.frame(indiv.comm.16sR0) #load in the pres/abs table
-  comm_presabs[comm_presabs > 0] <- 1 #change all rel abund to 1
-  comm_presabs <- tibble::rownames_to_column(comm_presabs, "UniqueID") #make rownames (UniqueID) into column
-} else if (tree.type=='rbcl'){
+comm_presabs <- as.data.frame(indiv.comm.16sR0) #load in the pres/abs table
+comm_presabs[comm_presabs > 0] <- 1 #change all rel abund to 1
+comm_presabs <- tibble::rownames_to_column(comm_presabs, "UniqueID") #make rownames (UniqueID) into column
 
-  meta <- spec16s %>%
-    select(all_of(meta_cols), Apidae, starts_with('RBCL')) %>%
-    na.omit() %>%
-    select(!starts_with('RBCL')) %>%
-    filter(Apidae == 1) #%>%
-  comm_presabs <- as.data.frame(indiv.comm.rbclR0) #load in the pres/abs table
-  comm_presabs[comm_presabs > 0] <- 1 #change all rel abund to 1
-  comm_presabs <- tibble::rownames_to_column(comm_presabs, "UniqueID") #ma
-
-}
 
 
 
@@ -410,7 +235,7 @@ if(tree.type=='16s'){
 # apis_tree <- phylotree_heatmap_byGenus(physeq16sR0, meta, "Apis", comm_presabs, apis_sites, all_levels=TRUE, do_collapse=FALSE, tree.type='16s')
 # apis_tree
 # 
-apis_tree2 <- phylotree_heatmap_byGenus(physeq16sR0, meta, "Apis", comm_presabs, apis_sites, all_levels=TRUE, do_collapse = TRUE, tree.type='16s')
+apis_tree2 <- phylotree_heatmap_byGenus(physeq16sR0, meta, "Apis mellifera", comm_presabs, apis_sites, all_levels=TRUE, do_collapse = TRUE)
 apis_tree2
 # 
 # apis_tree_drop6 <- phylotree_heatmap_byGenus(physeq16sR0, meta, "Apis", comm_presabs, apis_sites, all_levels=FALSE, levels_to_drop='D_6', do_collapse=TRUE, tree.type='16s')
@@ -418,47 +243,6 @@ apis_tree2
 # 
 # apis_tree_drop5 <- phylotree_heatmap_byGenus(physeq16sR0, meta, "Apis", comm_presabs, apis_sites, all_levels=FALSE, levels_to_drop=c('D_6', 'D_5'), do_collapse = TRUE, tree.type='16s')
 # apis_tree_drop5
-
-
-#apis RBCL
-tree.type <- 'rbcl'
-#tree.type <- '16s'
-
-if(tree.type=='16s'){
-  meta <- spec16s %>%
-    select(all_of(meta_cols), Apidae, starts_with('16s')) %>%
-    na.omit() %>%
-    select(!starts_with('X16s')) %>%
-    filter(Apidae == 1) #%>%
-  
-  comm_presabs <- as.data.frame(indiv.comm.16sR0) #load in the pres/abs table
-  comm_presabs[comm_presabs > 0] <- 1 #change all rel abund to 1
-  comm_presabs <- tibble::rownames_to_column(comm_presabs, "UniqueID") #make rownames (UniqueID) into column
-} else if (tree.type=='rbcl'){
-  
-  meta <- spec16s %>%
-    select(all_of(meta_cols), Apidae, starts_with('RBCL')) %>%
-    na.omit() %>%
-    #select(!starts_with('RBCL')) %>%
-    filter(Apidae == 1) #%>%
-  comm_presabs <- as.data.frame(indiv.comm.rbclR0) #load in the pres/abs table
-  comm_presabs[comm_presabs > 0] <- 1 #change all rel abund to 1
-  comm_presabs <- tibble::rownames_to_column(comm_presabs, "UniqueID") #ma
-  
-}
-
-
-apis_tree_rbcl <- phylotree_heatmap_byGenus(tree.object=physeqRBCLR0,
-                                            metadata=meta,
-                                            this.genus="Apis",
-                                            presAbsTable=comm_presabs,
-                                            site.order=apis_sites,
-                                            all_levels=TRUE,
-                                            #levels_to_drop=c('species', 'genus'),
-                                            do_collapse = TRUE,
-                                            tree.type='rbcl')
-apis_tree_rbcl
-
 
 
 
