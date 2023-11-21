add43961d2f7920016642f6bfed1e050af6187d2
hayesrebecca <rhayes7@uoregon.edu>
2023-11-21 15:41:11 -0800

    adding in model diagnostic checks to troubleshoot.. having convergence issues

--- analysis/microbiome/1communityMicrobes.R
+++ analysis/microbiome/1communityMicrobes.R
@@ -7,10 +7,12 @@ setwd("analysis/microbiome/")
 
 rm(list=ls())
 
+run.diagnostics = TRUE
 
 library(picante)
 library(bayesplot)
 library(brms)
+library(performance)
 
 ## all of the variables that are explanatory variables and thus need
 ## to be centered
@@ -22,7 +24,8 @@ vars_yearsr <- c("MeanFloralAbundance",
 vars_sp <- c("MeanITD",
              "rare.degree")
 
-variables.to.log <- "rare.degree"
+variables.to.log <- c("rare.degree",
+                      "MeanFloralAbundance")
 
 source("src/misc_microbe.R")
 source("src/misc.R")
@@ -31,7 +34,8 @@ source("src/standardize_weights.R")
 source("src/init_microbe.R")
 source("src/writeResultsTable.R")
 source("src/runParasiteModels.R")
-source("src/plant_poll_models_microbe.R")
+source("src/runPlotFreqModelDiagnostics.R")
+
 
 ncores <- 1
 
@@ -59,11 +63,112 @@ unique(spec.net$GenusSpecies[spec.net$Apidae == 1 &
                                is.na(spec.net$MeanITD)])
 
 #dropping genus species with NA -- 
-spec.net <- spec.net[!is.na(spec.net$GenusSpecies),]
+#spec.net <- spec.net[!is.na(spec.net$GenusSpecies),]
+
 
 
 ## **********************************************************
-## Parasite models set up
+## Flower abundance
+## **********************************************************
+
+## flower abundance variables 
+flower.abund.vars <- c("Year",
+                       "SRDoy",
+                       "I(SRDoy^2)",
+                       "Lat",
+                       "(1|Site)")
+
+flower.abund.x <- paste(flower.abund.vars, collapse="+")
+flower.abund.y <- "MeanFloralAbundance | weights(Weights)"
+formula.flower.abund <- as.formula(paste(flower.abund.y, "~",flower.abund.x))
+                                         
+
+#VegAbund check
+freq.formula.flower.abund <- as.formula(paste("MeanFloralAbundance", "~", flower.abund.x ))
+
+#for this_data, use spec.net[spec.net$Weights==1,] to incorporate weights into frequentist models
+if(run.diagnostics){
+  freq.model.flower.abund <- run_plot_freq_model_diagnostics(
+    freq.formula.flower.abund,
+    spec.net[spec.net$Weights==1,],
+    this_family = 'gaussian')
+  
+  ggsave(freq.model.flower.abund,
+         file="figures/SI_VegAbundModelDiagnostics.pdf",
+         height=8, width=11)
+}
+
+## **********************************************************
+## Flower diversity
+## **********************************************************
+
+
+## flower abundance variables 
+flower.div.vars <- c("Year",
+                       "SRDoy",
+                       "I(SRDoy^2)",
+                       "Lat",
+                       "(1|Site)")
+
+flower.div.x <- paste(flower.div.vars, collapse="+")
+flower.div.y <- "MeanFloralDiversity | weights(Weights)"
+formula.flower.div <- as.formula(paste(flower.div.y, "~",flower.div.x))
+
+
+#Vegdiv check
+freq.formula.flower.div <- as.formula(paste("MeanFloralDiversity", "~", flower.div.x ))
+
+#for this_data, use spec.net[spec.net$Weights==1,] to incorporate weights into frequentist models
+if(run.diagnostics){
+  freq.model.flower.div <- run_plot_freq_model_diagnostics(
+    freq.formula.flower.div,
+    spec.net[spec.net$Weights==1,],
+    this_family = 'gaussian')
+  
+  ggsave(freq.model.flower.div,
+         file="figures/SI_VegDivModelDiagnostics.pdf",
+         height=8, width=11)
+}
+
+## **********************************************************
+## Bee abundance
+## **********************************************************
+## bombus abund
+formula.bombus.abund <- formula(Net_BombusAbundance | weights(Weights)~
+                                  MeanFloralAbundance + Year +
+                                  SRDoy + I(SRDoy^2) +
+                                  Lat + 
+                                  (1|Site)
+)
+## HB abund
+formula.HB.abund <- formula(Net_HBAbundance | weights(Weights)~
+                              MeanFloralAbundance +  Year +
+                              SRDoy + I(SRDoy^2) +
+                              Lat +
+                              (1|Site)
+)
+## bee abund
+formula.bee.abund <- formula(Net_NonBombusHBAbundance | weights(Weights)~
+                               MeanFloralAbundance +  Year +
+                               SRDoy + I(SRDoy^2) +
+                               Lat +
+                               (1|Site)
+)
+
+
+## **********************************************************
+## Bee diversity
+## **********************************************************
+formula.bee.div <- formula(Net_BeeDiversity | weights(Weights)~
+                             MeanFloralDiversity +
+                             Lat + Year +
+                             SRDoy + I(SRDoy^2) +
+                             (1|Site)
+)
+
+
+## **********************************************************
+## Microbe models set up
 ## **********************************************************
 ## Multi species models
 xvars.multi.species <-  c("Net_NonBombusHBAbundance",
@@ -106,142 +211,44 @@ r2 <- rstantools::bayes_R2(fit.community)
 save(fit.community, spec.net, r2,
      file="saved/communityFit.Rdata")
 
-## **********************************************************
-## Model 1.3: formula for bee community effects on parasitism
-## **********************************************************
-
-get_genus_PD_formula <- function(input.df, genus){
-  genus.microbes <- input.df[input.df$Genus == genus, ]
-  
-  
-  genus.PD <- apply(genus.microbes[,microbes], 1, function(x){
-    this.bee <- x[x > 0]
-    this.tree <- prune.sample(t(this.bee), tree.16s)
-    pd(t(this.bee), this.tree, include.root = FALSE)
-  })
-  
-  genus.PD <- do.call(rbind, genus.PD)
-  
-  genus.microbes <- cbind(genus.microbes, genus.PD)
-  
-  input.df <- merge(input.df, genus.microbes, all.x=TRUE)
-
-  formula.microbes <- formula(genus.PD ~
-                                PollAbundance*FloralDiversity +
-                                PollDiversity +
-                                FloralAbundance +
-                                (1|Site)
-  )
-}
-
-#Apis
-apis.formula <- get_genus_PD_formula(spec, 'Apis')
-#Bombus
-bombus.formula <- get_genus_PD_formula(spec, 'Bombus')
-#Megachile
-megachile.formula <- get_genus_PD_formula(spec, 'Megachile')
-#Anthophora
-anthophora.formula <- get_genus_PD_formula(spec, 'Anthophora')
-
-
-
-## **********************************************************
-## Community models
-## **********************************************************
-## convert to brms format
-bf.fabund <- bf(formula.flower.abund)
-bf.fdiv <- bf(formula.flower.div)
-bf.babund <- bf(formula.bee.abund)
-bf.bdiv <- bf(formula.bee.div)
-bf.apis <- bf(apis.formula)
-bf.bombus <- bf(bombus.formula)
-bf.megachile <- bf(megachile.formula)
-bf.anthophora <- bf(anthophora.formula)
 
 ## **********************************************************
 ## Model 1 community effects on gut microbe phylo distance
 ## **********************************************************
 
 
+microbe.vars <- xvars.multi.species
 
-#Apis model
+microbe.x <- paste(microbe.vars, collapse="+")
+microbe.y <- "PD | weights(WeightsPar)"
+formula.microbe <- as.formula(paste(microbe.y, "~",
+                                     microbe.x))
 
-apis_bform <- bf.fabund + bf.fdiv + bf.babund + bf.bdiv + bf.apis +
+
+bf.microbe <- bf(formula.microbe)
+
+#combine forms
+bform <- bf.fabund + bf.fdiv +
+  bf.babund + bf.bombusabund + bf.HBabund +
+  bf.bdiv  +    
+  bf.microbe +
   set_rescor(FALSE)
 
 ## run model
-apis_fit <- brm(apis_bform, spec,
-           cores=ncores,
-           iter = 10^3,
-           chains = 2,
-           thin=1,
-           init=0,
-           control = list(adapt_delta = 0.99))
+fit.microbe <- brm(bform , spec.microbes,
+                  cores=ncores,
+                  iter = 10000,
+                  chains =1,
+                  thin=1,
+                  init=0,
+                  open_progress = FALSE,
+                  control = list(adapt_delta = 0.99),
+                  save_pars = save_pars(all = TRUE))
 
-write.ms.table(apis_fit, "apis")
-
-#Bombus model
-bombus_bform <- bf.fabund + bf.fdiv + bf.babund + bf.bdiv + bf.bombus +
-  set_rescor(FALSE)
-
-## run model
-bombus_fit <- brm(bombus_bform, spec,
-                cores=ncores,
-                iter = 10^3,
-                chains = 2,
-                thin=1,
-                init=0,
-                control = list(adapt_delta = 0.99))
-
-
-write.ms.table(bombus_fit, "bombus")
-
-#Megachile
-
-megachile_bform <- bf.fabund + bf.fdiv + bf.babund + bf.bdiv + bf.megachile +
-  set_rescor(FALSE)
-
-## run model
-megachile_fit <- brm(megachile_bform, spec,
-                cores=ncores,
-                iter = 10^3,
-                chains = 2,
-                thin=1,
-                init=0,
-                control = list(adapt_delta = 0.99))
-
-write.ms.table(megachile_fit, "megachile")
-
-#Anthophora
-anthophora_bform <- bf.fabund + bf.fdiv + bf.babund + bf.bdiv + bf.anthophora +
-  set_rescor(FALSE)
-
-## run model
-anthophora_fit <- brm(anthophora_bform, spec,
-                cores=ncores,
-                iter = 10^3,
-                chains = 2,
-                thin=1,
-                init=0,
-                control = list(adapt_delta = 0.99))
-
-write.ms.table(anthophora_fit, "anthophora")
-
-#write.ms.table(fit, "microbes")
-
-
-#save(fit, spec,
-#     file="saved/microbesFitMod.Rdata")
-## dignostic figures
-#mcmc_trace(fit)
-#ggsave("figures_diagnostics_microbes.pdf",
-#       height=11, width=8.5)
-
-
-
-
-
-
-
-
-
+write.ms.table(fit.microbe,
+               sprintf("full_microbe_%s_%s",
+                       species.group="all", parasite="none"))
+r2loo <- loo_R2(fit.microbe)
+r2 <- rstantools::bayes_R2(fit.microbe)
+save(fit.microbe, spec.net, r2, r2loo,
+     file="saved/fullMicrobeFit.Rdata")
