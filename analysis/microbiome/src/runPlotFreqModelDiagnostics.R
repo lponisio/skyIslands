6ff91b88b79403c983bdaf42f0d7195dfa82c6b1
hayesrebecca <rhayes7@uoregon.edu>
2023-11-27 15:15:19 -0800

    added additional model family capabilities to function

--- analysis/microbiome/src/runPlotFreqModelDiagnostics.R
+++ analysis/microbiome/src/runPlotFreqModelDiagnostics.R
@@ -1,6 +1,11 @@
 
 ## funciton to run frequentist version of brms models and plot diagnostics
 
+#currently works with all families included in ?family, as well as zero inflated neg binomial, negbinomial, Gamma, inverse gaussian, poisson, quasibinomial
+# will need to add other families as needed 
+
+
+
 run_plot_freq_model_diagnostics <- function(this_formula, #brms model formula
                             this_data, #data frame, subsetted to correct weights!
                             num_chains=1, 
@@ -31,8 +36,8 @@ run_plot_freq_model_diagnostics <- function(this_formula, #brms model formula
     this_model_output <- glmer(this_formula, data=this_data, family=Gamma)
     diagnostic.plots <- plot(check_model(this_model_output, panel = TRUE))
   } else if (this_family=='negbinomial') {
-    # this_model_output <- glmmadmb(this_formula, data=this_data, zeroInflation=FALSE, family='nbinom')
-    # diagnostic.plots <- plot(check_model(this_model_output, panel = TRUE))
+    this_model_output <- glmmadmb(this_formula, data=this_data, zeroInflation=FALSE, family='nbinom')
+    diagnostic.plots <- plot(check_model(this_model_output, panel = TRUE))
   } else if (this_family == 'Gamma'){
     #run model
     this_model_output <- glmer(this_formula, data=this_data, family=Gamma(link=log))
@@ -40,13 +45,35 @@ run_plot_freq_model_diagnostics <- function(this_formula, #brms model formula
     # return a list of single plots
     diagnostic.plots <- plot(check_model(this_model_output, 
                                          panel = TRUE))
-    } 
-  # else if (this_family=='Gamma') {
-  #   this_model_output <- glmmadmb(this_formula, data=this_data, zeroInflation=FALSE, family='gamma')
-  #   this_pp <- pp_check(this_model_output)
-  #   #diagnostic.plots <- plot(check_model(this_model_output, panel = TRUE))
-  #   this_pp
-  # }
+  } else if (this_family == 'inverse.gaussian'){
+    #run model
+    this_model_output <- brms::brm(this_formula,
+                                   data = this_data, 
+                                   chains = num_chains, 
+                                   iter = num_iter, family=this_family)
+    # return a list of single plots
+    diagnostic.plots <- plot(check_model(this_model_output, 
+                                         panel = TRUE))
+  } else if (this_family == 'poisson'){
+    #run model
+    this_model_output <- brms::brm(this_formula,
+                                   data = this_data, 
+                                   chains = num_chains, 
+                                   iter = num_iter, family=this_family)
+    # return a list of single plots
+    diagnostic.plots <- plot(check_model(this_model_output, 
+                                         panel = TRUE), type = "discrete_dots")
+  } 
+  else if (this_family == 'quasibinomial'){
+    #run model
+    this_model_output <- brms::brm(this_formula,
+                                   data = this_data, 
+                                   chains = num_chains, 
+                                   iter = num_iter, family=this_family)
+    # return a list of single plots
+    diagnostic.plots <- plot(check_model(this_model_output, 
+                                         panel = TRUE))
+  } 
   
   diagnostic.plots
 }
