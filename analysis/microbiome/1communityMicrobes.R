b17997f08f545c3dbe9718e4c9282aee9d0848ac
hayesrebecca <rhayes7@uoregon.edu>
2023-12-05 19:47:00 -0800

    running second version of microbe model w/ regular abundance weights, previously was log abund weights

--- analysis/microbiome/1communityMicrobes.R
+++ analysis/microbiome/1communityMicrobes.R
@@ -314,11 +314,15 @@ microbe.vars <-  c("BeeAbundance",
 
 microbe.x <- paste(microbe.vars, collapse="+")
 microbe.y <- "PD | weights(LogWeightsAbund)"
+microbe.y2 <- "PD | weights(WeightsAbund)"
 formula.microbe <- as.formula(paste(microbe.y, "~",
                                      microbe.x))
+formula.microbe2 <- as.formula(paste(microbe.y2, "~",
+                                    microbe.x))
 
 
 bf.microbe <- bf(formula.microbe)
+bf.microbe2 <- bf(formula.microbe2)
 
 #combine forms
 bform <- bf.fabund +
@@ -328,8 +332,16 @@ bform <- bf.fabund +
   bf.microbe +
   set_rescor(FALSE)
 
+#combine forms
+bform2 <- bf.fabund +
+  bf.fdiv +
+  bf.tot.babund +
+  bf.tot.bdiv  +
+  bf.microbe2 +
+  set_rescor(FALSE)
+
 ## run full model
-fit.microbe <- brm(bform , spec.net,
+fit.microbe2 <- brm(bform2 , spec.net,
                   cores=ncores,
                   iter = 10000,
                   chains =1,
@@ -340,244 +352,244 @@ fit.microbe <- brm(bform , spec.net,
                   save_pars = save_pars(all = TRUE),
                   data2 = list(phylo_matrix=phylo_matrix))
 
-write.ms.table(fit.microbe, "full_microbe")
-r2loo <- loo_R2(fit.microbe)
-r2 <- rstantools::bayes_R2(fit.microbe)
-save(fit.microbe, spec.net, r2, r2loo,
+write.ms.table(fit.microbe2, "full_microbe2")
+r2loo2 <- loo_R2(fit.microbe2)
+r22 <- rstantools::bayes_R2(fit.microbe2)
+save(fit.microbe2, spec.net, r22, r2loo2,
      file="saved/fullMicrobeFit.Rdata")
 
 ## run bombus model
-microbe.bombus.vars <- c("BeeAbundance",
-                                  "BeeDiversity", "Lat", #check this doesn't make VIF high
-                                  "MeanFloralDiversity", "MeanITD", 
-                                  "(1|Site)", "rare.degree", "(1|gr(GenusSpecies, cov = phylo_matrix))") # add cov matrix for each genus
-
-
-
-microbe.bombus.x <- paste(microbe.bombus.vars, collapse="+")
-microbe.bombus.y <- "PD | weights(LogWeightsAbund)"
-formula.microbe.bombus <- as.formula(paste(microbe.bombus.y, "~",
-                                    microbe.bombus.x))
-
-
-bf.microbe.bombus <- bf(formula.microbe.bombus)
-
-#combine forms
-bform.bombus <- bf.fabund +
-  bf.fdiv +
-  bf.tot.babund +
-  bf.tot.bdiv  +
-  bf.microbe.bombus +
-  set_rescor(FALSE)
-
-fit.microbe.bombus <- brm(bform.bombus , spec.bombus,
-                   cores=ncores,
-                   iter = 10000,
-                   chains =1,
-                   thin=1,
-                   init=0,
-                   open_progress = FALSE,
-                   control = list(adapt_delta = 0.99),
-                   save_pars = save_pars(all = TRUE),
-                   data2 = list(phylo_matrix=phylo_matrix))
-
-write.ms.table(fit.microbe.bombus, "bombus_microbe")
-r2loo.bombus <- loo_R2(fit.microbe.bombus)
-r2.bombus <- rstantools::bayes_R2(fit.microbe.bombus)
-save(fit.microbe.bombus, spec.bombus, r2.bombus, r2loo.bombus,
-     file="saved/fullMicrobeBombusFit.Rdata")
-
-## run apis model
-microbe.apis.vars <- c("BeeAbundance",
-                         "BeeDiversity", "Lat", #check this doesn't make VIF high
-                         "MeanFloralDiversity",# "MeanITD", 
-                         "(1|Site)", "rare.degree"#, "(1|gr(GenusSpecies, cov = phylo_matrix))") # add cov matrix for each genus
-)
-
-
-microbe.apis.x <- paste(microbe.apis.vars, collapse="+")
-microbe.apis.y <- "PD | weights(LogWeightsAbund)"
-formula.microbe.apis <- as.formula(paste(microbe.apis.y, "~",
-                                           microbe.apis.x))
-
-
-bf.microbe.apis <- bf(formula.microbe.apis)
-
-#combine forms
-bform.apis <- bf.fabund +
-  bf.fdiv +
-  bf.tot.babund +
-  bf.tot.bdiv  +
-  bf.microbe.apis +
-  set_rescor(FALSE)
-
-fit.microbe.apis <- brm(bform.apis , spec.apis,
-                          cores=ncores,
-                          iter = 10000,
-                          chains =1,
-                          thin=1,
-                          init=0,
-                          open_progress = FALSE,
-                          control = list(adapt_delta = 0.99),
-                          save_pars = save_pars(all = TRUE))
-
-write.ms.table(fit.microbe.apis, "apis_microbe")
-r2loo.apis <- loo_R2(fit.microbe.apis)
-r2.apis <- rstantools::bayes_R2(fit.microbe.apis)
-save(fit.microbe.apis, spec.apis, r2.apis, r2loo.apis,
-     file="saved/fullMicrobeApisFit.Rdata")
-
-## run melissodes model
-microbe.melissodes.vars <- c("BeeAbundance",
-                         "BeeDiversity", "Lat", #check this doesn't make VIF high
-                         "MeanFloralDiversity", #"MeanITD", 
-                         "(1|Site)", "rare.degree", "(1|gr(GenusSpecies, cov = phylo_matrix))") # add cov matrix for each genus
-
-
-
-microbe.melissodes.x <- paste(microbe.melissodes.vars, collapse="+")
-microbe.melissodes.y <- "PD | weights(LogWeightsAbund)"
-formula.microbe.melissodes <- as.formula(paste(microbe.melissodes.y, "~",
-                                           microbe.melissodes.x))
-
-
-bf.microbe.melissodes <- bf(formula.microbe.melissodes)
-
-#combine forms
-bform.melissodes <- bf.fabund +
-  bf.fdiv +
-  bf.tot.babund +
-  bf.tot.bdiv  +
-  bf.microbe.melissodes +
-  set_rescor(FALSE)
-
-fit.microbe.melissodes <- brm(bform.melissodes , spec.melissodes,
-                          cores=ncores,
-                          iter = 10000,
-                          chains =1,
-                          thin=1,
-                          init=0,
-                          open_progress = FALSE,
-                          control = list(adapt_delta = 0.99),
-                          save_pars = save_pars(all = TRUE))
-
-write.ms.table(fit.microbe.melissodes, "melissodes_microbe")
-r2loo.melissodes <- loo_R2(fit.microbe.melissodes)
-r2.melissodes <- rstantools::bayes_R2(fit.microbe.melissodes)
-save(fit.microbe.melissodes, spec.melissodes, r2.melissodes, r2loo.melissodes,
-     file="saved/fullMicrobeMelissodesFit.Rdata")
-
-
-
-## **********************************************************
-## Diagnostics
-## **********************************************************
-#VegAbund check
-if(run.diagnostics){
-# freq.formula.flower.abund <- as.formula(paste("MeanFloralAbundance", "~", flower.abund.x ))
+# microbe.bombus.vars <- c("BeeAbundance",
+#                                   "BeeDiversity", "Lat", #check this doesn't make VIF high
+#                                   "MeanFloralDiversity", "MeanITD", 
+#                                   "(1|Site)", "rare.degree", "(1|gr(GenusSpecies, cov = phylo_matrix))") # add cov matrix for each genus
 # 
-# #for this_data, use spec.net[spec.net$Weights==1,] to incorporate weights into frequentist models
+# 
+# 
+# microbe.bombus.x <- paste(microbe.bombus.vars, collapse="+")
+# microbe.bombus.y <- "PD | weights(LogWeightsAbund)"
+# formula.microbe.bombus <- as.formula(paste(microbe.bombus.y, "~",
+#                                     microbe.bombus.x))
+# 
+# 
+# bf.microbe.bombus <- bf(formula.microbe.bombus)
+# 
+# #combine forms
+# bform.bombus <- bf.fabund +
+#   bf.fdiv +
+#   bf.tot.babund +
+#   bf.tot.bdiv  +
+#   bf.microbe.bombus +
+#   set_rescor(FALSE)
+# 
+# fit.microbe.bombus <- brm(bform.bombus , spec.bombus,
+#                    cores=ncores,
+#                    iter = 10000,
+#                    chains =1,
+#                    thin=1,
+#                    init=0,
+#                    open_progress = FALSE,
+#                    control = list(adapt_delta = 0.99),
+#                    save_pars = save_pars(all = TRUE),
+#                    data2 = list(phylo_matrix=phylo_matrix))
+# 
+# write.ms.table(fit.microbe.bombus, "bombus_microbe")
+# r2loo.bombus <- loo_R2(fit.microbe.bombus)
+# r2.bombus <- rstantools::bayes_R2(fit.microbe.bombus)
+# save(fit.microbe.bombus, spec.bombus, r2.bombus, r2loo.bombus,
+#      file="saved/fullMicrobeBombusFit.Rdata")
+# 
+# ## run apis model
+# microbe.apis.vars <- c("BeeAbundance",
+#                          "BeeDiversity", "Lat", #check this doesn't make VIF high
+#                          "MeanFloralDiversity",# "MeanITD", 
+#                          "(1|Site)", "rare.degree"#, "(1|gr(GenusSpecies, cov = phylo_matrix))") # add cov matrix for each genus
+# )
+# 
+# 
+# microbe.apis.x <- paste(microbe.apis.vars, collapse="+")
+# microbe.apis.y <- "PD | weights(LogWeightsAbund)"
+# formula.microbe.apis <- as.formula(paste(microbe.apis.y, "~",
+#                                            microbe.apis.x))
+# 
+# 
+# bf.microbe.apis <- bf(formula.microbe.apis)
+# 
+# #combine forms
+# bform.apis <- bf.fabund +
+#   bf.fdiv +
+#   bf.tot.babund +
+#   bf.tot.bdiv  +
+#   bf.microbe.apis +
+#   set_rescor(FALSE)
+# 
+# fit.microbe.apis <- brm(bform.apis , spec.apis,
+#                           cores=ncores,
+#                           iter = 10000,
+#                           chains =1,
+#                           thin=1,
+#                           init=0,
+#                           open_progress = FALSE,
+#                           control = list(adapt_delta = 0.99),
+#                           save_pars = save_pars(all = TRUE))
+# 
+# write.ms.table(fit.microbe.apis, "apis_microbe")
+# r2loo.apis <- loo_R2(fit.microbe.apis)
+# r2.apis <- rstantools::bayes_R2(fit.microbe.apis)
+# save(fit.microbe.apis, spec.apis, r2.apis, r2loo.apis,
+#      file="saved/fullMicrobeApisFit.Rdata")
+# 
+# ## run melissodes model
+# microbe.melissodes.vars <- c("BeeAbundance",
+#                          "BeeDiversity", "Lat", #check this doesn't make VIF high
+#                          "MeanFloralDiversity", #"MeanITD", 
+#                          "(1|Site)", "rare.degree", "(1|gr(GenusSpecies, cov = phylo_matrix))") # add cov matrix for each genus
+# 
+# 
+# 
+# microbe.melissodes.x <- paste(microbe.melissodes.vars, collapse="+")
+# microbe.melissodes.y <- "PD | weights(LogWeightsAbund)"
+# formula.microbe.melissodes <- as.formula(paste(microbe.melissodes.y, "~",
+#                                            microbe.melissodes.x))
+# 
+# 
+# bf.microbe.melissodes <- bf(formula.microbe.melissodes)
+# 
+# #combine forms
+# bform.melissodes <- bf.fabund +
+#   bf.fdiv +
+#   bf.tot.babund +
+#   bf.tot.bdiv  +
+#   bf.microbe.melissodes +
+#   set_rescor(FALSE)
+# 
+# fit.microbe.melissodes <- brm(bform.melissodes , spec.melissodes,
+#                           cores=ncores,
+#                           iter = 10000,
+#                           chains =1,
+#                           thin=1,
+#                           init=0,
+#                           open_progress = FALSE,
+#                           control = list(adapt_delta = 0.99),
+#                           save_pars = save_pars(all = TRUE))
+# 
+# write.ms.table(fit.microbe.melissodes, "melissodes_microbe")
+# r2loo.melissodes <- loo_R2(fit.microbe.melissodes)
+# r2.melissodes <- rstantools::bayes_R2(fit.microbe.melissodes)
+# save(fit.microbe.melissodes, spec.melissodes, r2.melissodes, r2loo.melissodes,
+#      file="saved/fullMicrobeMelissodesFit.Rdata")
+# 
+# 
+# 
+# ## **********************************************************
+# ## Diagnostics
+# ## **********************************************************
+# #VegAbund check
 # if(run.diagnostics){
-#   freq.model.flower.abund <- run_plot_freq_model_diagnostics(
-#     freq.formula.flower.abund,
-#     spec.net[spec.net$Weights==1,],
-#     this_family = 'gaussian')
-#   
-#   ggsave(freq.model.flower.abund,
-#          file="figures/diagnostics/SI_VegAbundModelDiagnostics.pdf",
-#          height=8, width=11)
-# }
-# 
-# #Vegdiv check
-# freq.formula.flower.div <- as.formula(paste("MeanFloralDiversity", "~", flower.div.x ))
-# 
-# #for this_data, use spec.net[spec.net$Weights==1,] to incorporate weights into frequentist models
-# if(run.diagnostics){
-#   freq.model.flower.div <- run_plot_freq_model_diagnostics(
-#     freq.formula.flower.div,
-#     spec.net[spec.net$Weights==1,],
-#     this_family = 'gaussian')
-#   
-#   ggsave(freq.model.flower.div,
-#          file="figures/diagnostics/SI_VegDivModelDiagnostics.pdf",
-#          height=8, width=11)
-# }
-# 
-# # bee abund check
-# freq.formula.tot.bee.abund <- as.formula(paste("BeeAbundance", "~", tot.bee.abund.x ))
-# 
-# ##for this_data, use spec.net[spec.net$Weights==1,] to incorporate weights into frequentist models
-# if(run.diagnostics){
-#   freq.model.tot.bee.abund <- run_plot_freq_model_diagnostics(
-#     freq.formula.tot.bee.abund,
-#     spec.net[spec.net$Weights==1,],
-#     this_family = "gaussian")
-#   
-#   ggsave(freq.model.tot.bee.abund,
-#          file="figures/diagnostics/SI_TotalBeeAbundModelDiagnostics.pdf",
-#          height=8, width=11)
-# }
-# 
+# # freq.formula.flower.abund <- as.formula(paste("MeanFloralAbundance", "~", flower.abund.x ))
+# # 
+# # #for this_data, use spec.net[spec.net$Weights==1,] to incorporate weights into frequentist models
+# # if(run.diagnostics){
+# #   freq.model.flower.abund <- run_plot_freq_model_diagnostics(
+# #     freq.formula.flower.abund,
+# #     spec.net[spec.net$Weights==1,],
+# #     this_family = 'gaussian')
+# #   
+# #   ggsave(freq.model.flower.abund,
+# #          file="figures/diagnostics/SI_VegAbundModelDiagnostics.pdf",
+# #          height=8, width=11)
+# # }
+# # 
+# # #Vegdiv check
+# # freq.formula.flower.div <- as.formula(paste("MeanFloralDiversity", "~", flower.div.x ))
+# # 
+# # #for this_data, use spec.net[spec.net$Weights==1,] to incorporate weights into frequentist models
+# # if(run.diagnostics){
+# #   freq.model.flower.div <- run_plot_freq_model_diagnostics(
+# #     freq.formula.flower.div,
+# #     spec.net[spec.net$Weights==1,],
+# #     this_family = 'gaussian')
+# #   
+# #   ggsave(freq.model.flower.div,
+# #          file="figures/diagnostics/SI_VegDivModelDiagnostics.pdf",
+# #          height=8, width=11)
+# # }
+# # 
 # # # bee abund check
-# # freq.formula.net.bee.abund <- as.formula(paste("Net_BeeAbundance", "~", net.bee.abund.x ))
+# # freq.formula.tot.bee.abund <- as.formula(paste("BeeAbundance", "~", tot.bee.abund.x ))
 # # 
 # # ##for this_data, use spec.net[spec.net$Weights==1,] to incorporate weights into frequentist models
 # # if(run.diagnostics){
-# #   freq.model.net.bee.abund <- run_plot_freq_model_diagnostics(
-# #     freq.formula.net.bee.abund,
+# #   freq.model.tot.bee.abund <- run_plot_freq_model_diagnostics(
+# #     freq.formula.tot.bee.abund,
 # #     spec.net[spec.net$Weights==1,],
 # #     this_family = "gaussian")
 # #   
-# #   ggsave(freq.model.net.bee.abund,
-# #          file="figures/diagnostics/SI_NetBeeAbundModelDiagnostics.pdf",
+# #   ggsave(freq.model.tot.bee.abund,
+# #          file="figures/diagnostics/SI_TotalBeeAbundModelDiagnostics.pdf",
 # #          height=8, width=11)
 # # }
-# 
-# # # bee div check
-# # freq.formula.bee.div <- as.formula(paste("Net_BeeDiversity", "~", bee.div.x ))
+# # 
+# # # # bee abund check
+# # # freq.formula.net.bee.abund <- as.formula(paste("Net_BeeAbundance", "~", net.bee.abund.x ))
+# # # 
+# # # ##for this_data, use spec.net[spec.net$Weights==1,] to incorporate weights into frequentist models
+# # # if(run.diagnostics){
+# # #   freq.model.net.bee.abund <- run_plot_freq_model_diagnostics(
+# # #     freq.formula.net.bee.abund,
+# # #     spec.net[spec.net$Weights==1,],
+# # #     this_family = "gaussian")
+# # #   
+# # #   ggsave(freq.model.net.bee.abund,
+# # #          file="figures/diagnostics/SI_NetBeeAbundModelDiagnostics.pdf",
+# # #          height=8, width=11)
+# # # }
+# # 
+# # # # bee div check
+# # # freq.formula.bee.div <- as.formula(paste("Net_BeeDiversity", "~", bee.div.x ))
+# # # 
+# # # ##for this_data, use spec.net[spec.net$Weights==1,] to incorporate weights into frequentist models
+# # # if(run.diagnostics){
+# # #   freq.model.bee.div <- run_plot_freq_model_diagnostics(
+# # #     freq.formula.bee.div,
+# # #     spec.net[spec.net$Weights==1,],
+# # #     this_family = "gaussian")
+# # #   
+# # #   ggsave(freq.model.bee.div,
+# # #          file="figures/diagnostics/SI_BeeDiversityModelDiagnostics.pdf",
+# # #          height=8, width=11)
+# # # }
+# # 
+# # # total bee div check
+# # freq.formula.tot.bee.div <- as.formula(paste("BeeDiversity", "~", tot.bee.div.x ))
 # # 
 # # ##for this_data, use spec.net[spec.net$Weights==1,] to incorporate weights into frequentist models
 # # if(run.diagnostics){
-# #   freq.model.bee.div <- run_plot_freq_model_diagnostics(
-# #     freq.formula.bee.div,
+# #   freq.model.tot.bee.div <- run_plot_freq_model_diagnostics(
+# #     freq.formula.tot.bee.div,
 # #     spec.net[spec.net$Weights==1,],
 # #     this_family = "gaussian")
 # #   
-# #   ggsave(freq.model.bee.div,
-# #          file="figures/diagnostics/SI_BeeDiversityModelDiagnostics.pdf",
+# #   ggsave(freq.model.tot.bee.div,
+# #          file="figures/diagnostics/SI_TotalBeeDivModelDiagnostics.pdf",
 # #          height=8, width=11)
 # # }
 # 
-# # total bee div check
-# freq.formula.tot.bee.div <- as.formula(paste("BeeDiversity", "~", tot.bee.div.x ))
+# # microbe check
+# freq.formula.microbe <- as.formula(paste("PD", "~", microbe.x ))
 # 
 # ##for this_data, use spec.net[spec.net$Weights==1,] to incorporate weights into frequentist models
 # if(run.diagnostics){
-#   freq.model.tot.bee.div <- run_plot_freq_model_diagnostics(
-#     freq.formula.tot.bee.div,
-#     spec.net[spec.net$Weights==1,],
-#     this_family = "gaussian")
+#   freq.model.microbe <- run_plot_freq_model_diagnostics(
+#     freq.formula.microbe,
+#     spec.net[spec.net$WeightsPar==1,],
+#     this_family = "gaussian",
+#     launch.shiny = FALSE,
+#     examine.pairs = FALSE)
 #   
-#   ggsave(freq.model.tot.bee.div,
-#          file="figures/diagnostics/SI_TotalBeeDivModelDiagnostics.pdf",
+#   ggsave(freq.model.microbe,
+#          file="figures/diagnostics/SI_MicrobeModelDiagnostics.pdf",
 #          height=8, width=11)
+#   }
 # }
-
-# microbe check
-freq.formula.microbe <- as.formula(paste("PD", "~", microbe.x ))
-
-##for this_data, use spec.net[spec.net$Weights==1,] to incorporate weights into frequentist models
-if(run.diagnostics){
-  freq.model.microbe <- run_plot_freq_model_diagnostics(
-    freq.formula.microbe,
-    spec.net[spec.net$WeightsPar==1,],
-    this_family = "gaussian",
-    launch.shiny = FALSE,
-    examine.pairs = FALSE)
-  
-  ggsave(freq.model.microbe,
-         file="figures/diagnostics/SI_MicrobeModelDiagnostics.pdf",
-         height=8, width=11)
-  }
-}
-
+# 
