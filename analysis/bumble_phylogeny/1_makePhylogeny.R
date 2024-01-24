7c239d4cb5565b881f309a147add7597f7b5a4cf
Nicole Martinez Llaurador <91992903+nmartinez14@users.noreply.github.com>
2024-01-24 12:35:43 -0800

    Code for making bee phylogeny and bombus only phylogeny

---
+++ analysis/bumble_phylogeny/1_makePhylogeny.R
@@ -0,0 +1,59 @@
+## Generate phylogeny of Bombus
+
+setwd("C:/Users/na_ma/Dropbox (University of Oregon)/skyIslands")
+
+setwd("analysis/bumble_phylogeny")
+rm(list=ls())
+
+## load tree from :
+## Henriquez Piskulich, Patricia Andrea; Hugall, Andrew F.; Stuart-Fox, Devi (2023). 
+## A supermatrix phylogeny of the world’s bees (Hymenoptera: Anthophila) [Dataset]. 
+## Dryad. https://doi.org/10.5061/dryad.80gb5mkw1
+
+phylo <- ape::read.tree("../../data/BEE_mat7_fulltree.nwk")
+
+##clean up unwanted portion of labels
+pattern <- "(_n\\d+m\\d+_[A-Za-z0-9]+)?$"
+phylo$tip.label <- gsub(pattern, "", phylo$tip.label)
+
+## replace underscore with space
+phylo$tip.label <- gsub("_", " ", phylo$tip.label)
+
+## i think we need to drop the tips that aren't in our dataset
+## changing sp labels to match phylogeny
+species_to_keep <- na.omit(unique(spec.net$GenusSpecies))
+print(length(species_to_keep))
+
+
+#the 6 species getting dropped are still present at this step
+
+phylo_tips <- phylo$tip.label
+print(length(phylo_tips))
+
+#only keep tips that match our species
+phylo <- ape::keep.tip(phylo, species_to_keep[species_to_keep %in% phylo_tips])
+print(length(phylo$tip.label))
+
+## Calculate covariance matrix
+phylo_matrix <- ape::vcv.phylo(phylo)
+save(phylo, phylo_matrix, file = "../../data/community_phylogeny.Rdata")
+###############################################################################
+
+## Do a bombus only phylogeny
+species_to_keep <- na.omit(unique(spec.net$GenusSpecies[spec.net$Genus == "Bombus"]))
+print(length(species_to_keep))
+
+
+#the 6 species getting dropped are still present at this step
+
+phylo_tips <- phylo$tip.label
+print(length(phylo_tips))
+
+#only keep tips that match our species
+phylo <- ape::keep.tip(phylo, species_to_keep[species_to_keep %in% phylo_tips])
+print(length(phylo$tip.label))
+
+## Calculate covariance matrix
+phylo_matrix <- ape::vcv.phylo(phylo)
+
+save(phylo, phylo_matrix, file = "../../data/bombus_phylogeny.Rdata")
