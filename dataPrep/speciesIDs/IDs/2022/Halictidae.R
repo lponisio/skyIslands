8d090c111f375733e688afcd835ecef509d05b47
hlewack3 <156253826+hlewack3@users.noreply.github.com>
2024-03-22 16:53:42 -0700

    Update Halictidae.R
    added HBL 3/20/24
    36522	Lasioglossum Rudisense	f
    36643	Lasioglossum Rudisense	f
    36519	Lasioglossum Rudisense	f
    37007	Lasioglossum Rudisense	f
    31247	lassioglossum obnubilum	f
    34342	lassioglossum obnubilum	f
    31231	lassioglossum obnubilum	f
    33578	lassioglossum obnubilum	f
    34278	lassioglossum obnubilum	f
    31115	lassioglossum obnubilum	f
    38364	lassioglossum rudisense	m
    34468	lassioglossum rudisense	m
    31793	lassioglossum rudisense	m
    34440	lassioglossum rudisense	m
    31127	lassioglossum rudisense	m
    31124	lassioglossum rudisense	m
    31125	lassioglossum rudisense	m
    34330	lassioglossum rudisense	m
    32698	lassioglossum rudisense	m
    32699	lassioglossum rudisense	m

--- dataPrep/speciesIDs/IDs/2022/Halictidae.R
+++ dataPrep/speciesIDs/IDs/2022/Halictidae.R
@@ -83,26 +83,26 @@ sp.ids <- list(
     ##                           Determiner ="",
     ##                           temp.id= c()),
 
-     ##     Sphecodes_sp_b_m= list(Genus="Sphecodes", SubGenus ="",
-    ##                           Species="sp.",
-    ##                           SubSpecies="b", Sex="m",
-    ##                           Author = "",
-    ##                           Determiner ="J.L. Neff",
-    ##                           temp.id= c(31928)),
+          Sphecodes_sp_b_m= list(Genus="Sphecodes", SubGenus ="",
+                               Species="sp.",
+                               SubSpecies="b", Sex="m",
+                               Author = "",
+                               Determiner ="J.L. Neff",
+                               temp.id= c(31928)),
 
-      ##     Sphecodes_sp_b_f= list(Genus="Sphecodes", SubGenus ="",
-    ##                           Species="sp.",
-    ##                           SubSpecies="b", Sex="f",
-    ##                           Author = "",
-    ##                           Determiner ="J.L. Neff",
-    ##                           temp.id= c(33979, 34341, 33274)),
+           Sphecodes_sp_b_f= list(Genus="Sphecodes", SubGenus ="",
+                               Species="sp.",
+                               SubSpecies="b", Sex="f",
+                               Author = "",
+                               Determiner ="J.L. Neff",
+                               temp.id= c(33979, 34341, 33274)),
 
-      ##     Sphecodes_sp_a_f= list(Genus="Sphecodes", SubGenus ="",
-    ##                           Species="sp.",
-    ##                           SubSpecies="a", Sex="f",
-    ##                           Author = "",
-    ##                           Determiner ="J.L. Neff",
-    ##                           temp.id= c(30936, 30938, 31600, 30771, 30802, 31259)),
+           Sphecodes_sp_a_f= list(Genus="Sphecodes", SubGenus ="",
+                               Species="sp.",
+                               SubSpecies="a", Sex="f",
+                               Author = "",
+                               Determiner ="J.L. Neff",
+                               temp.id= c(30936, 30938, 31600, 30771, 30802, 31259)),
 
     Dufourea_maura_f= list(Genus="Dufourea", SubGenus ="",
                            Species="maura", SubSpecies="",
@@ -246,14 +246,17 @@ sp.ids <- list(
                                   Author = "Cockerell",
                                   Determiner = "J.L. Neff",
                                   temp.id=c(36523, 37110, 36469, 37049, 37498,
-                                            37512, 36785, 37106, 37112)),
+                                            37512, 36785, 37106, 37112, 
+                                           #added HBL 3/20/24
+                                           36522, 36643, 36519, 37007)),
 
-  ## Lasioglossum_ruidosense_m= list(Genus="Lasioglossum", SubGenus
-  ##                                 ="Dialictus", Species="ruidosense",
-  ##                                 SubSpecies="cf.", Sex="m",
-  ##                                 Author = "Cockerell",
-  ##                                 Determiner = "",
-  ##                                 temp.id=c()),
+  #added HBL 3/20/24
+    Lasioglossum_ruidosense_m= list(Genus="Lasioglossum", SubGenus
+                                   ="Dialictus", Species="ruidosense",
+                                   SubSpecies="cf.", Sex="m",
+                                   Author = "Cockerell",
+                                   Determiner = "",
+                                   temp.id=c(38364, 34468, 31793, 34440, 31127, 31124, 31125, 34330, 32698, 32699)),
 
 
   Lasioglossum_pacatum_viridatulum_f=
@@ -274,14 +277,15 @@ sp.ids <- list(
   ##          Determiner="",
   ##          temp.id=c()),
 
-  ## Lasioglossum_obnubilum_f= list(Genus="Lasioglossum",
-  ##                                SubGenus ="Dialictus",
-  ##                                Species="obnubilum",
-  ##                                SubSpecies="",
-  ##                                Sex="f",
-  ##                                Author = "Sandhouse",
-  ##                                Determiner = "",
-  ##                                temp.id= c()),
+  #added hbl 3/20/24
+    Lasioglossum_obnubilum_f= list(Genus="Lasioglossum",
+                                  SubGenus ="Dialictus",
+                                  Species="obnubilum",
+                                  SubSpecies="",
+                                  Sex="f",
+                                  Author = "Sandhouse",
+                                  Determiner = "",
+                                  temp.id= c(31247, 34342, 31231, 33578, 34278, 31115)),
 
   ## Lasioglossum_obnubilum_m= list(Genus="Lasioglossum",
   ##                                SubGenus ="Dialictus",
@@ -310,12 +314,12 @@ sp.ids <- list(
 
 
 
-  ## Lasioglossum_lusorium_f= list(Genus="Lasioglossum", SubGenus
-  ##                               ="Sphecodogastra", Species="lusorium",
-  ##                               SubSpecies="", Sex="f",
-  ##                               Author = "Cresson",
-  ##                               Determiner = "J.L. Neff",
-  ##                               temp.id=  c(32695)),
+   Lasioglossum_lusorium_f= list(Genus="Lasioglossum", SubGenus
+                                 ="Sphecodogastra", Species="lusorium",
+                                 SubSpecies="", Sex="f",
+                                 Author = "Cresson",
+                                 Determiner = "J.L. Neff",
+                                 temp.id=  c(32695)),
 
   ## Lasioglossum_sp_m= list(Genus="Lasioglossum", SubGenus
   ##                         ="Sphecodogastra", Species="sp.",
@@ -324,19 +328,19 @@ sp.ids <- list(
   ##                         Determiner = "",
   ##                         temp.id=  c())
 
-  ## Lasioglossum_rupticristum_m= list(Genus="Lasioglossum", SubGenus
-  ##                         ="", Species="rupticristum",
-  ##                         SubSpecies="", Sex="m",
-  ##                         Author = "McGinley",
-  ##                         Determiner = "J.L. Neff",
-  ##                         temp.id=  c(38345, 38376, 38378, 30645, 32884, 31130, 31113, 37077, 33590, 33476, 30651))
+   Lasioglossum_rupticristum_m= list(Genus="Lasioglossum", SubGenus
+                           ="", Species="rupticristum",
+                           SubSpecies="", Sex="m",
+                           Author = "McGinley",
+                           Determiner = "J.L. Neff",
+                           temp.id=  c(38345, 38376, 38378, 30645, 32884, 31130, 31113, 37077, 33590, 33476, 30651))
 
-  ## Lasioglossum_rupticristum_f= list(Genus="Lasioglossum", SubGenus
-  ##                         ="", Species="rupticristum",
-  ##                         SubSpecies="", Sex="f",
-  ##                         Author = "McGinley",
-  ##                         Determiner = "J.L. Neff",
-  ##                         temp.id=  c(38355, 38371, 38344, 38388, 38347, 38324, 38386, 38377))
+   Lasioglossum_rupticristum_f= list(Genus="Lasioglossum", SubGenus
+                           ="", Species="rupticristum",
+                           SubSpecies="", Sex="f",
+                           Author = "McGinley",
+                           Determiner = "J.L. Neff",
+                           temp.id=  c(38355, 38371, 38344, 38388, 38347, 38324, 38386, 38377))
 
 
   ## Lasioglossum_cooleyi_f= list(Genus="Lasioglossum", SubGenus
