f8267594c782dfba9ac769e941807a83d56954f3
Nicole Martinez Llaurador <91992903+nmartinez14@users.noreply.github.com>
2024-04-30 14:54:22 -0700

    Created a map for the sky island sites

--- dataPrep/spatial.R
+++ dataPrep/spatial.R
@@ -1,20 +1,23 @@
 library(sf)
 library(ggplot2)
-library(raster)
-
+library(terra)
+library(ggspatial)
+library(basemaps)
+library(tidyverse)
 library(gdalUtilities)
 library(rgdal)
 
 library(spatstat)
-#library(maptools)
-source("lab_paths.R")
-#setwd("~/Dropbox/skyIslands_saved/data/spatial")
 
-sp.dir <- local.path
+source("lab_paths.R")
+setwd(file.path(local.path))
+
+#sp.dir <- file.path(local.path, "skyIslands_saved/spatial")
 
 geo <- read.csv("skyIslands_saved/data/relational/original/geography.csv")
 
 crs.std <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"
+
 
 
 
@@ -60,13 +63,53 @@ nm.az.r <- raster(file.path(sp.dir,
 ## ***********************************************************************
 ## sample sites
 ## ***********************************************************************
+## Create dataframe with the points and save as a shapefile
+#sites <- SpatialPointsDataFrame(coords = cbind(geo$Long, geo$Lat),
+#                               data = geo,
+#                              proj4string = CRS(crs.std))
 
-sites <- SpatialPointsDataFrame(coords = cbind(geo$Long, geo$Lat),
-                               data = geo,
-                               proj4string = CRS(crs.std))
+#writeOGR(sites, file.path(sp.dir, "sites.shp"), "sites",
+#         driver="ESRI Shapefile")
 
-writeOGR(sites, file.path(sp.dir, "sites.shp"), "sites",
-         driver="ESRI Shapefile")
+## Create shapefile
+sites_sf<- st_as_sf(geo,
+         coords = c("Long", "Lat"),
+         crs = crs.std)
+ggplot() +
+  geom_sf(data = sites_sf) +
+  ggtitle("Map of Plot Locations")
+
+## Get basemap
+
+## set defaults for the basemap
+set_defaults(map_service = "esri", map_type = "usa_topo_maps")
+## Change the CRS to match the basemaps
+site_points <- st_transform(sites_sf, crs = st_crs(3857))
+## Create a new bounding box to avoid points in the corners
+bbox_new <- st_bbox(site_points) # current bounding box
+
+xrange <- bbox_new$xmax - bbox_new$xmin # range of x values
+yrange <- bbox_new$ymax - bbox_new$ymin # range of y values
+
+ bbox_new[1] <- bbox_new[1] - (0.1 * xrange) # xmin - left
+ bbox_new[3] <- bbox_new[3] + (0.1 * xrange) # xmax - right
+ bbox_new[2] <- bbox_new[2] - (0.1 * yrange) # ymin - bottom
+bbox_new[4] <- bbox_new[4] + (0.1 * yrange) # ymax - top
+bbox_new <- bbox_new %>%  # take the bounding box make it a spatial object
+  st_as_sfc()
+
+map <-ggplot() +
+  basemap_gglayer(bbox_new) + # Use new bbox to download the basemap
+  geom_sf(data = site_points, color = "orange", fill = "transparent") +
+  coord_sf(xlim = st_coordinates(bbox_new)[c(1,2),1], # min & max of x values
+           ylim = st_coordinates(bbox_new)[c(2,3),2], expand = FALSE) +
+  scale_fill_identity()+ 
+  xlab("Longitude") + ylab("Latitude") +
+  annotation_north_arrow(location = "tl", style = north_arrow_fancy_orienteering)+
+  annotation_scale(location = "br")+
+  theme_minimal()
+
+ggsave("skyIslands_sites.pdf", path = "skyIslands_saved/spatial", height=4, width=5)
 
 ## ***********************************************************************
 ## all the sw
