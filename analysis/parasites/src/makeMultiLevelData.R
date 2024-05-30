743c00b9b15a30078fcac51b1576f4fb374a1156
Nicole Martinez Llaurador <91992903+nmartinez14@users.noreply.github.com>
2024-05-30 15:04:21 -0700

    Removed weights from being hardcoded in the function to being an argument

--- analysis/parasites/src/makeMultiLevelData.R
+++ analysis/parasites/src/makeMultiLevelData.R
@@ -1,11 +1,13 @@
 
-makeDataMultiLevel <- function(indiv.data, site.col, year.col="Year"){
+makeDataMultiLevel <- function(indiv.data, site.col, year.col="Year", weight.col.name="Weights",
+                               site.id.col.name="SiteIDs"){
     ## split data by year
     indiv.data.split <- split(indiv.data, indiv.data[, year.col])
 
     ## maybe in the future this will need to be an sapply
     out.indiv.data <- lapply(indiv.data.split, addWeightCol,
-                             site.col=site.col)
+                             site.col=site.col,  weight.col.name= weight.col.name,
+                             site.id.col.name=site.id.col.name)
 
     out.indiv.data <- do.call(rbind, out.indiv.data)
 
@@ -14,15 +16,17 @@ makeDataMultiLevel <- function(indiv.data, site.col, year.col="Year"){
 }
 
 
-addWeightCol <- function(each.year.dat, site.col){
+addWeightCol <- function(each.year.dat, site.col, 
+                         weight.col.name="Weights",
+                         site.id.col.name="SiteIDs"){
     site.ids <- unlist(tapply(each.year.dat[, site.col],
                               each.year.dat[, site.col],
                               function(x) 1:length(x)))
 
 
     names(site.ids) <- NULL
-    each.year.dat$SiteIDs <- site.ids
-    each.year.dat$Weights <- each.year.dat$SiteIDs
-    each.year.dat$Weights[each.year.dat$Weights > 1] <- 0
+    each.year.dat[, site.id.col.name] <- site.ids
+    each.year.dat[, weight.col.name] <- each.year.dat[, site.id.col.name]
+    each.year.dat[, weight.col.name][each.year.dat[weight.col.name] > 1] <- 0
     return(each.year.dat)
 }
