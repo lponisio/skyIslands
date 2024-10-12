510de7ce43c8b564e286a8db3dfbe266a128bb2c
hayesrebecca <rhayes7@uoregon.edu>
2024-10-12 16:27:51 -0700

    feat: added combination network turnover plot function, need to do a bit of rearranging to improve model visibility

--- analysis/turnover/src/networkTurnover.R
+++ analysis/turnover/src/networkTurnover.R
@@ -163,65 +163,115 @@ plot_network_turnover_mod_single <- function(mod1,
   return(list(plot_obj, model_geodist))
 }
 
-plot_network_turnover_mod_compare <- function(mod1, 
-                                              mod2, 
-                                             this.network,
-                                             network_type,
-                                             this.effect,
-                                             this.resp,
-                                             label
+plot_network_turnover_mod_compare <- function(mod1,
+                                              mod2,
+                                              this.network1,
+                                              this.network2,
+                                              network_type1,
+                                              network_type2,
+                                              this.effect,
+                                              this.resp,
+                                              label
 ){
   
+  # prep cond effects mod 1
   
-  mod_summary <- write.summ.table(mod1)
-  model_geodist <- mod_summary[rownames(mod_summary) == "GeoDist",]
+  mod_summary1 <- write.summ.table(mod1)
+  model_geodist1 <- mod_summary1[rownames(mod_summary1) == "GeoDist",]
   
-  if(network_type == "Obligate") {
-    point_color <- "darkgreen"
-    if(model_geodist$Pgt0 >= 0.95){
-      ribbon_color <- "darkgreen"
-    } else if (model_geodist$Pgt0 <= 0.05) {
-      ribbon_color <- "darkgreen"
-    } else {ribbon_color <- NA}
+  if(network_type1 == "Obligate") {
+    point_color1 <- "darkgreen"
+    if(model_geodist1$Pgt0 >= 0.95){
+      ribbon_color1 <- "darkgreen"
+    } else if (model_geodist1$Pgt0 <= 0.05) {
+      ribbon_color1 <- "darkgreen"
+    } else {ribbon_color1 <- NA}
   }
   
-  if(network_type == "Transient") {
-    point_color <- "darkorange"
-    if(model_geodist$Pgt0 >= 0.95){
-      ribbon_color <- "darkorange"
-    } else if (model_geodist$Pgt0 <= 0.05) {
-      ribbon_color <- "darkorange"
-    } else {ribbon_color <- NA}
+  if(network_type1 == "Transient") {
+    point_color1 <- "darkorange"
+    if(model_geodist1$Pgt0 >= 0.95){
+      ribbon_color1 <- "darkorange"
+    } else if (model_geodist1$Pgt0 <= 0.05) {
+      ribbon_color1 <- "darkorange"
+    } else {ribbon_color1 <- NA}
   }
   
   # Extract the data from conditional_effects
-  cond_effects_data <- conditional_effects(mod1, effects = this.effect, resp = this.resp, plot = FALSE)
-  plot_data <- cond_effects_data[[this.effect]]
+  cond_effects_data1 <- conditional_effects(mod1, effects = this.effect, resp = this.resp, plot = FALSE)
+  plot_data1 <- cond_effects_data1[[this.effect]]
   #browser()
+  
+  # now repeat for mod 2
+  mod_summary2 <- write.summ.table(mod2)
+  model_geodist2 <- mod_summary2[rownames(mod_summary2) == "GeoDist",]
+  
+  if(network_type2 == "Obligate") {
+    point_color2 <- "darkgreen"
+    if(model_geodist2$Pgt0 >= 0.95){
+      ribbon_color2 <- "darkgreen"
+    } else if (model_geodist2$Pgt0 <= 0.05) {
+      ribbon_color2 <- "darkgreen"
+    } else {ribbon_color2 <- NA}
+  }
+  
+  if(network_type2 == "Transient") {
+    point_color2 <- "darkorange"
+    if(model_geodist2$Pgt0 >= 0.95){
+      ribbon_color2 <- "darkorange"
+    } else if (model_geodist2$Pgt0 <= 0.05) {
+      ribbon_color2 <- "darkorange"
+    } else {ribbon_color2 <- NA}
+  }
+  
+  # Extract the data from conditional_effects
+  cond_effects_data2 <- conditional_effects(mod2, effects = this.effect, resp = this.resp, plot = FALSE)
+  plot_data2 <- cond_effects_data2[[this.effect]]
+  
   # Plot using ggplot2 for credible intervals with geom_ribbon
-  plot_obj <- ggplot(plot_data, aes(x = .data[[this.effect]], y = .data$estimate__)) +
+  plot_obj <- ggplot(plot_data1, aes(x = .data[[this.effect]], y = .data$estimate__)) +
     # Add ribbons for the 95%, 80%, and 50% credible intervals
     geom_ribbon(aes(ymin = lower__, ymax = upper__), alpha = 0.2, 
-                fill = ribbon_color,
-                color = point_color, linetype='dotted') +
+                fill = ribbon_color1,
+                color = point_color1, linetype='dotted') +
     geom_ribbon(aes(ymin = lower__ + 0.1 * (upper__ - lower__),
                     ymax = upper__ - 0.1 * (upper__ - lower__)),
                 alpha = 0.3, 
-                fill=ribbon_color, 
-                color = point_color, linetype='dashed') +
+                fill=ribbon_color1, 
+                color = point_color1, linetype='dashed') +
     geom_ribbon(aes(ymin = lower__ + 0.25 * (upper__ - lower__),
                     ymax = upper__ - 0.25 * (upper__ - lower__)),
                 alpha = 0.4, 
-                fill=ribbon_color,
-                color = point_color, linetype='solid') +
+                fill=ribbon_color1,
+                color = point_color1, linetype='solid') +
     #Add line for the estimates
-    geom_line(data = plot_data, color = 'black', linewidth=2.5, aes(x = .data[[this.effect]], y = .data$estimate__)) +
+    geom_line(data = plot_data1, color = 'black', linewidth=2.5, aes(x = .data[[this.effect]], y = .data$estimate__)) +
     #Add line for the estimates
-    geom_line(data = plot_data, color = point_color, linewidth=2, aes(x = .data[[this.effect]], y = .data$estimate__)) +
+    geom_line(data = plot_data1, color = point_color1, linewidth=2, aes(x = .data[[this.effect]], y = .data$estimate__)) +
     # Add points for original data
-    geom_point(data = this.network, aes(x = .data[[this.effect]], y = .data[[this.resp]]),
-               fill = point_color, alpha = 0.9,color="black", pch=21, cex=3) +
-    # Labels and theme
+    geom_point(data = this.network1, aes(x = .data[[this.effect]], y = .data[[this.resp]]),
+               fill = point_color1, alpha = 0.9,color="black", pch=21, cex=3) +
+    # Add ribbons for the 95%, 80%, and 50% credible intervals
+    geom_ribbon(data=plot_data2, aes(ymin = lower__, ymax = upper__), alpha = 0.2, 
+                fill = ribbon_color2,
+                color = point_color2, linetype='dotted') +
+    geom_ribbon(data=plot_data2, aes(ymin = lower__ + 0.1 * (upper__ - lower__),
+                    ymax = upper__ - 0.1 * (upper__ - lower__)),
+                alpha = 0.3, 
+                fill=ribbon_color2, 
+                color = point_color2, linetype='dashed') +
+    geom_ribbon(data=plot_data2, aes(ymin = lower__ + 0.25 * (upper__ - lower__),
+                    ymax = upper__ - 0.25 * (upper__ - lower__)),
+                alpha = 0.4, 
+                fill=ribbon_color2,
+                color = point_color2, linetype='solid') +
+    #Add line for the estimates
+    geom_line(data = plot_data2, color = 'black', linewidth=2.5, aes(x = .data[[this.effect]], y = .data$estimate__)) +
+    #Add line for the estimates
+    geom_line(data = plot_data2, color = point_color2, linewidth=2, aes(x = .data[[this.effect]], y = .data$estimate__)) +
+    # Add points for original data
+    geom_point(data = this.network2, aes(x = .data[[this.effect]], y = .data[[this.resp]]),
+               fill = point_color2, alpha = 0.9,color="black", pch=21, cex=3) +
     theme_classic()  +
     labs(x = "Geographic Distance (km)", y = label,
          fill = "Credible Interval") +
@@ -231,7 +281,6 @@ plot_network_turnover_mod_compare <- function(mod1,
           axis.title.y = element_text(size=16),
           text = element_text(size=16),
           legend.position = "none")
-  
-  return(list(plot_obj, model_geodist))
-  
+  #TODO add together model geodist objs for output
+  return(list(plot_obj, model_geodist1, model_geodist2))
 }
