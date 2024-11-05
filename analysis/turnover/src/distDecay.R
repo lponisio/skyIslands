10cb23132c9eb4b7d34bb33f69f421d1de0e8e6d
hayesrebecca <rhayes7@uoregon.edu>
2024-11-05 10:40:13 -0800

    feat: added custom plotting functions for distance decay to src folder

--- analysis/turnover/src/distDecay.R
+++ analysis/turnover/src/distDecay.R
@@ -20,42 +20,6 @@ spec16s <- spec.net %>%
 ##Species abundance dissimilarity matrix: created using a distance measure, i.e. Bray-curtis dissimilarity. This is the same type of dissimilarity matrix used when conducting an ANOSIM test or when making an NMDS plot
 #Geographic distance matrix: the physical distance between sites (i.e. Haversine distance)
 
-
-custom.plot.decay <- function(x, xlim = c(0, max(x$data.x)), ylim = c(0, 1), add = FALSE, 
-                              remove.dots = FALSE, col = "black", pch = 1, lty = 1, lwd = 5, 
-                              cex = 1, ...) 
-{
-  if (!inherits(x, "decay")) {
-    stop("The input is not a distance-decay model fitted with decay.model().", 
-         call. = TRUE)
-  }
-  data <- data.frame(as.vector(x$data.x), as.vector(x$data.y))
-  if (!remove.dots) {
-    pch = pch
-  }
-  else {
-    pch = ""
-  }
-  if (!add) {
-    plot(x$data.x, x$data.y, xlim = xlim, ylim = ylim, col = col, pch = pch, cex = cex, 
-         ...)
-  }
-  if (add) {
-    points(x$data.x, x$data.y, xlim = xlim, ylim = ylim, col = col, pch = pch, 
-           cex = cex, ...)
-  }
-  switch(x$y.type, similarities = {
-    lines(fitted(x$model)[order(x$data.x)] ~ sort(x$data.x), 
-          col = col, lty = lty, lwd = lwd, ...)
-  }, dissimilarities = {
-    lines(1 - fitted(x$model)[order(x$data.x)] ~ sort(x$data.x), 
-          col = col, lty = lty, lwd = lwd, ...)
-  })
-}
-
-
-
-
 ## This function takes as input a data frame and a bee genus and filters the data frame to
 ## just bees of the input genus, computes bray curtis dissimilarity of their 16s communities,
 ## computes haversine distance matrix of sample sites, performs a mantel test to determine
@@ -118,72 +82,82 @@ genusspecies.decay.model <- function(data, type, which, model.type){
 }
 
 
-ppint.dis <- function(spec.dat,
-                      types= c("GenusSpecies", "PlantGenusSpecies", "Int"),
-                      c.dist, sub="all", method.dist="gower"){
-    comm.dis <- function(spec.dat, type){
-        browser()
-        prep.comm <- aggregate(spec.dat[, type],
-                               list(Site=spec.dat$Site,
-                                    Year=spec.dat$Year,
-                                    GenusSpecies=spec.dat[, type]),
-                               length)
-        prep.comm$Nyears  <- 20
-        prep.comm$Nyears[prep.comm$Site %in% c("HM", "SM", "PL", "VC")] <- 10
-        prep.comm$AveAbund <- prep.comm$x/prep.comm$Nyears
-        comm <- samp2site.spp(prep.comm$Site,
-                              prep.comm$GenusSpecies, prep.comm$AveAbund)
-        comm.dis <- as.matrix(vegdist(comm, method.dist, diag= TRUE))
-        c.comm <- comm.dis[lower.tri(comm.dis)]
-        return(c.comm)
-    }
-    browser()
-    ##pollinator dissimilarity
-    c.pol <-comm.dis(spec.dat, types[1])
-    ##plant dissimilarity
-    c.plant <- comm.dis(spec.dat, types[2])
-    ##interaction dissimilarity
-    c.int <-  comm.dis(spec.dat, types[3])
-    c.dist <- scale(log(c.dist))
-    ##linear models
-    lm.pol <- lm(c.pol~c.dist)
-    lm.plant <- lm(c.plant~c.dist)
-    lm.int <- lm(c.int~c.dist)
-
-    return(list(summary(lm.pol), summary(lm.plant), summary(lm.int)))
+plot_decay_ggplot_single <- function(x,
+                                     xlab,
+                                     ylab,
+                                     mod1color='navy',
+                                     col = "black",
+                                     lty = "solid",
+                                     lwd = 1.5,
+                                     cex = 1) {
+  #browser()
+  # Extract data and fitted values
+  data <- data.frame(x$data.x, x$data.y)
+  model <- x$model
+  fitted_values <- data.frame(fitted(model)[order(data$x.data.x)])
+  sorted_data <- data[order(data$x.data.x), ]
+  #browser()
+  # Create the ggplot object
+  p <- ggplot(data, aes(x = x.data.x, y = x.data.y)) +
+    geom_jitter(fill = mod1color, alpha = 0.1 , color="black", pch=21, cex=3) +
+    geom_line(aes(x = sorted_data$x.data.x, y = (1 - fitted_values$fitted.model..order.data.x.data.x..)),
+              color = 'black', linewidth=2.5,) +
+    geom_line(aes(x = sorted_data$x.data.x, y = (1 - fitted_values$fitted.model..order.data.x.data.x..)), color = mod1color, linewidth=2) +
+    labs(x=xlab, y=ylab) +
+    theme_classic() +
+    theme(axis.title.x = element_text(size=16),
+          axis.title.y = element_text(size=16),
+          text = element_text(size=16))
+  
+  return(p)
 }
 
 
 
+# Plot with ggplot2
+plot_decay_ggplot_single(bombus_model, xlab="Geographic Distance (km)", ylab='Pairwise Bray-Curtis Dissimilarity')
 
-f <- function(){
-
-    f.curve <- function(x, intercept, slope){
-        y <- intercept + x * slope
-        return(y)
-    }
-    layout(1)
-    plot(c.pol~c.dist, pch =16, col="darkolivegreen", ylim=c(0,1),
-         xlab="distance", ylab="dissimilarity")
-    points(c.plant~c.dist, pch=16, col="dodgerblue")
-    points(c.int~c.dist, pch=16, col="darkred")
-    legend("bottomright", legend= c("Pollinators", "Plants",
-                                    "Interactions"), col=c("darkolivegreen",
-                                                           "dodgerblue","darkred"),
-           pch=16, bty="n")
-
-    curve(f.curve(x=x, intercept =
-                           coef(summary(lm.pol))[1,1],
-                  slope=coef(summary(lm.pol))[2,1]),
-          lwd=2, col="darkolivegreen", add=TRUE)
-    curve(f.curve(x=x, intercept =
-                           coef(summary(lm.plant))[1,1],
-                  slope=coef(summary(lm.plant))[2,1]),
-          lwd=2, col="dodgerblue", add=TRUE)
-
-    curve(f.curve(x=x, intercept =
-                           coef(summary(lm.int))[1,1],
-                  slope=coef(summary(lm.int))[2,1]),
-          lwd=2, col="darkred", add=TRUE)
-
+plot_decay_ggplot_combined <- function(x,
+                                       z,
+                                       xlab,
+                                       ylab,
+                                       mod1color='navy',
+                                       mod2color='gold',
+                                       col = "black",
+                                       lty1,
+                                       lty2,
+                                       lwd = 1.5,
+                                       cex = 1) {
+  
+  # Extract data and fitted values
+  data1 <- data.frame(x$data.x, x$data.y)
+  model1 <- x$model
+  fitted_values1 <- data.frame(fitted(model1)[order(data1$x.data.x)])
+  sorted_data1 <- data1[order(data1$x.data.x), ]
+  
+  # Extract data and fitted values
+  data2 <- data.frame(z$data.x, z$data.y)
+  model2 <- z$model
+  fitted_values2 <- data.frame(fitted(model2)[order(data2$z.data.x)])
+  sorted_data2 <- data2[order(data2$z.data.x), ]
+  
+  #browser()
+  
+  # Create the ggplot object
+  p <- ggplot(data1, aes(x = x.data.x, y = x.data.y)) +
+    geom_jitter(data=data1, aes(x = x.data.x, y = x.data.y), fill = mod1color, alpha = 0.1 , color="black", pch=21, cex=3) +
+    geom_jitter(data=data2, aes(x = z.data.x, y = z.data.y), fill = mod2color, alpha = 0.3 , color="black", pch=21, cex=3) +
+    geom_line(aes(x = sorted_data1$x.data.x, y = (1 - fitted_values1$fitted.model1..order.data1.x.data.x..)),
+              color = 'black', linewidth=2.5,) +
+    geom_line(aes(x = sorted_data1$x.data.x, y = (1 - fitted_values1$fitted.model1..order.data1.x.data.x..)), color = mod1color, linewidth=2, linetype=lty1) +
+    geom_line(data=data2, aes(x = sorted_data2$z.data.x, y = (1 - fitted_values2$fitted.model2..order.data2.z.data.x..)),
+              color = 'black', linewidth=2.5,) +
+    geom_line(data=data2, aes(x = sorted_data2$z.data.x, y = (1 - fitted_values2$fitted.model2..order.data2.z.data.x..)), color = mod2color, linewidth=2, linetype=lty2) +
+    labs(x=xlab, y=ylab) +
+    theme_classic() +
+    theme(axis.title.x = element_text(size=16),
+          axis.title.y = element_text(size=16),
+          text = element_text(size=16))
+  
+  return(p)
 }
