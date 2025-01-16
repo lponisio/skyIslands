61dfd2e31578567ba2f045a73ea4276381330969
Alejandro Santillana <119963666+santillanaale@users.noreply.github.com>
2025-01-15 16:13:57 -0800

    fix: syntax

--- dataPrep/speciesIDs/IDs/2018/Butterflies.R
+++ dataPrep/speciesIDs/IDs/2018/Butterflies.R
@@ -1,4 +1,6 @@
-Hesperia_comma = list(Genus = "Hesperia", SubGenus="",Species =
+sp.ids <- list(
+    
+    Hesperia_comma = list(Genus = "Hesperia", SubGenus="",Species =
     "comma", SubSpecies = "", Sex = "", Author = "Linnaeus", Family =
     "Hesperiidae", Determiner="L.Ponisio", temp.id = c(24886)),
 
