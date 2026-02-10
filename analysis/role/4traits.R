d7c01252b82a7c827346e8cb3ce98570d512d401
jswanson3 <jswansonpdx@gmail.com>
2026-02-10 12:52:58 -0800

    Merging climate data to trait data

--- analysis/role/4traits.R
+++ analysis/role/4traits.R
@@ -16,9 +16,70 @@ library(ggplot2, quietly = TRUE)
 
 pol.traits <- read.csv(file = 'saved/traits/pol_beta_traits.csv')
 plant.traits <- read.csv(file = 'saved/traits/plant_beta_traits.csv')
+load('saved/results/pol_partnerVar_Site.Rdata')
 
+dim(spec.net)
 colnames(pol.traits)
 
+
+## --- calculate emergence and flight period, then average across columns species within a site and year --- ##
+spec <- spec.net %>% 
+  group_by(GenusSpecies, Year) %>% 
+  mutate(beeEmergence_start = min(Doy),
+  beeEmergence_end = max(Doy),
+  flight_period = beeEmergence_end - beeEmergence_start,
+    .groups = "drop") %>% 
+  group_by(GenusSpecies, Site, Year) %>%
+  summarise(across(where(is.numeric), \(x) mean(x, na.rm = TRUE)), 
+            .groups = "drop") %>% 
+  mutate(GenusSpeciesSiteYear = paste0(GenusSpecies, Site, Year))
+
+pol.beta <- beta.dist %>%
+  mutate(GenusSpeciesSiteYear = paste0(GenusSpecies, Site, Year))
+
+colnames(pol.beta)
+colnames(spec)
+
+## --- merge species level network metrics and trait data (keeps all rows from pol.beta.traits) ---##
+  pol.beta.traits <- spec %>%
+  right_join(pol.beta, by = "GenusSpeciesSiteYear")
+
+colnames(pol.beta.traits)
+
+write.csv(pol.beta.traits, file = 'saved/traits/pol_beta_traits.csv')
+
+## --- Annual climate variability (standard deviation) --- ##
+## --- Precipitation & Tempature --- ##
+setwd('../../../skyIslands_saved')
+climate <- read.csv("data/relational/original/climate.csv")
+colnames(climate)
+
+av.climate <- climate %>% 
+  group_by(Site, Year) %>% 
+  mutate(Av)
+
+
+## -- Assess sampling start date for each site by year -- ##
+spec.net %>% 
+  ggplot(aes(x = Doy, y = SampleRound)) +
+  geom_point() +
+  facet_wrap(Site~Year)
+
+
+
+
+
+
+
+
+
+colnames(pol.traits)
+colnames(plant.traits)
+
+#what are the difference in Precip and temp?
+hist(pol.traits$SpringPrecip)
+hist(pol.traits$CumulativePrecip)
+hist(pol.traits$RoundPrecip)
 
 pol.traits %>% 
   ggplot(aes(x = dist, y = nestedrank)) +
@@ -58,15 +119,13 @@ plant.traits %>%
 # phen <- phen %>% 
 #   group_by(GenusSpecies, Year) %>% 
 #   mutate(
-#     beeEmergence_start = min(Doy),
-#     beeEmergence_end = max(Doy),
-#     flight_period = beeEmergence_end - beeEmergence_start)
+    beeEmergence_start = min(Doy),
+    beeEmergence_end = max(Doy),
+    flight_period = beeEmergence_end - beeEmergence_start)
 # 
 # #average across all columns
 # phen.av <- phen %>% 
-#   group_by(GenusSpecies, Site, Year) %>%
-#   summarise(across(where(is.numeric), \(x) mean(x, na.rm = TRUE)),
-#             .groups = "drop") %>% 
+ 
 #   mutate(GenusSpeciesSiteYear = paste0(GenusSpecies, Site, Year))
 # 
 # 
