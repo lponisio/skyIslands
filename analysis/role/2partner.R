779cd54ca6a91ce6148bcdd932a52ac6601bdf68
jswanson3 <jswansonpdx@gmail.com>
2026-02-16 15:43:43 -0800

    feat: added code to merge partner variability, network, and trait data

--- analysis/role/2partner.R
+++ analysis/role/2partner.R
@@ -12,6 +12,7 @@ setwd('analysis/role')
 
 source('src/initialize_beta.R')
 
+
 ## ************************************************************
 ## beta diversity as variation between sites,
 ## centroid for each year
@@ -33,74 +34,24 @@ beta.dist <- makeBetaDataPretty()
 save(beta.dist, file=sprintf("saved/results/pol_partnerVar_%s.Rdata",
                              net.type))
 
-## *********************************************************
-## Create summary figures
-## *********************************************************
 
-## Plants
-## Beta calculated between years within a site (temporal)
-load('saved/results/plant_partnerVar_Year.Rdata')
-
-beta.dist %>% 
-  ggplot(
-  aes(y = reorder(GenusSpecies, dist), x = dist, color = Site)) +
-  geom_point() +
-  labs(x = "Partner variability", y = "Species") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-print(paste("Plant species", length(unique(beta.dist$GenusSpecies)))) #22 species
-unique(beta.dist$GenusSpecies)
-
-unique.species <- beta.dist %>%
-  separate(GenusSpecies, into = c("Genus", "Species"), sep = " ") %>% 
-  distinct(Genus, Species)
-
-write.csv(unique.species, file = "saved/traits/plantSpecies_partner.csv", row.names = FALSE)
-
-#Beta calculated between sites within a year (spatial)
-load('saved/results/plant_partnerVar_Site.Rdata')
-
-beta.dist %>% 
-  ggplot(
-    aes(y = reorder(GenusSpecies, dist), x = dist)) +
-  geom_point() +
-  labs(x = "Partner variability", y = "Species") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-print(paste("Plant species", length(unique(beta.dist$GenusSpecies)))) #48 species
-unique(beta.dist$GenusSpecies)
-
-
-## Pollinators
-## Beta calculated between years within a site (temporal)
-load('saved/results/pol_partnerVar_Year.Rdata')
-
-beta.dist %>% 
-  ggplot(
-    aes(y = reorder(GenusSpecies, dist), x = dist)) +
-  geom_point() +
-  labs(x = "Partner variability", y = "Species") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-print(paste("Plant species", length(unique(beta.dist$GenusSpecies)))) #58 species
-unique(beta.dist$GenusSpecies)
-
-unique.species <- beta.dist %>%
-  separate(GenusSpecies, into = c("Genus", "Species"), sep = " ") %>% 
-  distinct(Genus, Species)
-
-write.csv(unique.species, file = "saved/traits/polSpecies_partner.csv", row.names = FALSE)
-
-#Beta calculated between sites within a year (spatial)
+## calculate emergence and flight period ##
 load('saved/results/pol_partnerVar_Site.Rdata')
+load('../../data/spec_traits.Rdata')
 
-beta.dist %>% 
-  ggplot(
-    aes(y = reorder(GenusSpecies, dist), x = dist)) +
-  geom_point() +
-  labs(x = "Partner variability", y = "Species") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+spec <- spec.net %>% 
+  group_by(GenusSpecies, Year) %>% 
+  mutate(beeEmergence_start = min(Doy),
+         beeEmergence_end = max(Doy),
+         flight_period = beeEmergence_end - beeEmergence_start,
+         .groups = "drop") %>% 
+  mutate(GenusSpeciesSiteYear = paste0(GenusSpecies, Site, Year))
 
-print(paste("Plant species", length(unique(beta.dist$GenusSpecies)))) #47 species
-unique(beta.dist$GenusSpecies)
+pol.partner <- beta.dist %>%
+  mutate(GenusSpeciesSiteYear = paste0(GenusSpecies, Site, Year))
 
+## merge species level network metrics, role variability, trait data ##
+pol.beta.traits <- spec %>%
+  right_join(pol.partner, by = "GenusSpeciesSiteYear") # keeps all rows from pol.beta
+
+write.csv(pol.beta.traits, file = 'saved/traits/pol_partner_traits.csv')
