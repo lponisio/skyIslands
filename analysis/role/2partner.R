b332fb14d2cda6653fdd277ae3af8558d921d3b9
jswanson3 <jswansonpdx@gmail.com>
2026-02-17 11:01:51 -0800

    feat: modified code to calculate climate variability from april 1st to sept 30th

--- analysis/role/2partner.R
+++ analysis/role/2partner.R
@@ -41,10 +41,10 @@ load('../../data/spec_traits.Rdata')
 
 spec <- spec.net %>% 
   group_by(GenusSpecies, Year) %>% 
-  mutate(beeEmergence_start = min(Doy),
-         beeEmergence_end = max(Doy),
-         flight_period = beeEmergence_end - beeEmergence_start,
-         .groups = "drop") %>% 
+  mutate(PolEmergenceStart = min(Doy),
+         PolEmergenceEnd = max(Doy),
+         FlightPeriod = PolEmergenceEnd - PolEmergenceStart) %>%
+  ungroup() %>% 
   mutate(GenusSpeciesSiteYear = paste0(GenusSpecies, Site, Year))
 
 pol.partner <- beta.dist %>%
@@ -52,12 +52,29 @@ pol.partner <- beta.dist %>%
 
 ## merge species level network metrics, role variability, trait data ##
 pol.beta.traits <- spec %>%
-  right_join(pol.partner, by = "GenusSpeciesSiteYear") # keeps all rows from pol.beta
+  right_join(pol.partner %>% select(-SampleRound, -Site, -Year, -GenusSpecies), 
+             by = "GenusSpeciesSiteYear")
+
+## create vector with columns names and filter unwanted columns ##
+names <- c('SiteYearSR', 'SampleRound', 'SiteYear', 'Site', 'Year', 'Date', 'Lat', 'Long', 'Elev', 'Area', 'GenusSpecies',
+           'Sex', 'Order', 'Family', 'Genus', 'SubGenus', 'Species', 'SubSpecies',
+           'PlantGenus', 'PlantSpecies', 'PlantVar', 'PlantSubSpecies', 'State', 'County', 'Meadow', 'Forest',
+           'MtRange', 'PlantGenusSpecies', 'Int', 'Doy', 'PlantFamily', 'degree',
+           'normalised.degree', 'species.strength', 'interaction.push.pull', 'nestedrank',
+           'PDI', 'resource.range', 'species.specificity.index', 'PSI', 'node.specialisation.index.NSI',
+           'betweenness', 'weighted.betweenness', 'closeness', 'weighted.closeness', 'Fisher.alpha',
+           'partner.diversity', 'effective.partners', 'proportional.generality', 'proportional.similarity', 
+           'd', 'tot.int', 'niche.overlap', 'rare.degree', 'PollAbundance', 'BeeAbundance', 'SyrphidAbundance', 
+           'HBAbundance', 'BombusAbundance', 'NonBombusHBAbundance', 'PollRichness', 'BeeRichness',
+           'SyrphidRichness', 'BombusRichness', 'PollDiversity', 'BeeDiversity', 'SyrphidDiversity',
+           'BombusDiversity', 'VisitedFloralRichness', 'VisitedFloralDiversity', 'MeanFloralRichness',
+           'MeanFloralDiversity', 'MeanFloweringPlantDiversity', 'MeanFloweringPlantAbundance', 'Abundance', 'uniqSite',
+           'originalitySite', 'originalitySiteYearSR', 'BeeFDisSiteYearSR', 'BeeFEveSiteYearSR', 'uniqSiteYear',
+           'originalitySiteYear', 'BeeFDisSiteYear', 'BeeFEveSiteYear', 'PolEmergenceStart', 'PolEmergenceEnd',
+           'FlightPeriod', 'GenusSpeciesSiteYear', 'dist', 'Type')
+
+pol.beta.traits <- pol.beta.traits[, names]
 
 write.csv(pol.beta.traits, file = 'saved/traits/pol_partner_traits.csv')
 
 
-## create vector with columns and filter unwanted columns ##
-colnames(pol.beta.traits)
-
-names <- c('')
--- analysis/role/5climate.R
+++ analysis/role/5climate.R
@@ -1,5 +1,6 @@
 ######################################################################
 ## -- This script calculates annual climate variability -- ##
+## April 1st (first snow melt) to September 30th (last rain) ##
 #####################################################################
 setwd("C:/")
 source("lab_paths.R")
@@ -51,6 +52,7 @@ daily_temp_data <- daily_files %>%
 
 ## -- annual standard deviation by site and year -- ##
 annualPrecip.var <- daily_precip_data %>% 
+  filter(month(Date) >= 4 & month(Date) <= 9) %>%
   group_by(Site, Year) %>% 
   summarise(precipSD = sd(Precip, na.rm = TRUE))
 
@@ -76,6 +78,7 @@ daily_temp_data <- daily_temp_data %>%
 
 ## -- Calculate annual temp and temp anomoly variability -- ##
 annualTemp.var <- daily_temp_data %>% 
+  filter(month(Date) >= 4 & month(Date) <= 9) %>% 
   group_by(Site, Year) %>% 
   summarise(tempSD = sd(Tmean_normal, na.rm = TRUE),
             tempAnomSD = sd(Tmean_anomaly, na.rm = TRUE), .groups = "drop")
@@ -84,6 +87,5 @@ annualTemp.var <- daily_temp_data %>%
 climateVar <- annualPrecip.var %>%
   left_join(annualTemp.var, by = c("Site", "Year")) %>%
   select(Site, Year, precipSD, tempSD, tempAnomSD)
-getwd()
 
-write.csv(climateVar, file = "saved/traits/climateVariability.csv")
+write.csv(climateVar, file = "../skyIslands/analysis/role/saved/traits/climateVariability.csv")
