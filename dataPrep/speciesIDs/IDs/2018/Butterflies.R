f1158b01d6c1dbf2a9debf7514a96180529c8879
Alejandro Santillana <119963666+santillanaale@users.noreply.github.com>
2026-02-19 14:35:00 -0800

    Update Butterflies.R
    fix: duplicated IDs

--- dataPrep/speciesIDs/IDs/2018/Butterflies.R
+++ dataPrep/speciesIDs/IDs/2018/Butterflies.R
@@ -1,12 +1,4 @@
 sp.ids <- list(
-    
-    Hesperia_comma = list(Genus = "Hesperia", SubGenus="",Species =
-    "comma", SubSpecies = "", Sex = "", Author = "Linnaeus", Family =
-    "Hesperiidae", Determiner="L.Ponisio", temp.id = c(24886)),
-
- Hesperia_pahaska = list(Genus = "Hesperia", SubGenus="",Species =
-    "pahaska", SubSpecies = "", Sex = "", Author = "Leussler", Family
-    = "Hesperiidae", Determiner="L.Ponisio", temp.id = c(24264,24261,24805,24808)),
 
 Speyeria_atlantis = list(Genus = "Speyeria", SubGenus="", Species =
     "atlantis", SubSpecies = "", Sex = "", Author = "Edwards", Family
