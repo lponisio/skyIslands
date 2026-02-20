0f8eb2af79f070f105137fca961fea080f640ffb
Alejandro Santillana <119963666+santillanaale@users.noreply.github.com>
2026-02-20 12:57:41 -0800

    Update Butterflies.R

--- dataPrep/speciesIDs/IDs/2021/Butterflies.R
+++ dataPrep/speciesIDs/IDs/2021/Butterflies.R
@@ -73,9 +73,7 @@ sp.ids <- list(
 
   Colias_eurytheme = list(Genus = "Colias", SubGenus="", Species = "eurytheme",
     SubSpecies = "", Sex = "", Author = "Boisduval", Family =
-    "Pieridae", Determiner="J.F. Zorn", temp.id = c(36341,36345,36363,36364,36365,36378,36381,36398,36401,36402,36404,38406,38464,38472,38495,38499,38500,38524,38529,
-                                                     38531,38537,38545,38546,38575,38576,38579,38598,38599,38600,38601,38602,38603,38642,38645,38662,38778,38878,39013,
-                                                     39016,39052)),
+    "Pieridae", Determiner="J.F. Zorn", temp.id = c(38598)),
   
     Eurema_nicippe = list(Genus = "Eurema", SubGenus="",Species =
     "nicippe", SubSpecies = "", Sex = "", Author = "Cramer", Family =
